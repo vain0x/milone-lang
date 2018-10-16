fe737666d078452e27526ea15ba740f91d7e7fea
vain0x <vainzerox@gmail.com>
2018-10-16 18:58:45 +0900

    chore: Collect test targets auto

--- boot/MiloneLang.Tests/CPrintingTests.fs
+++ boot/MiloneLang.Tests/CPrintingTests.fs
@@ -6,34 +6,30 @@ open MiloneLang.Assets
 open Xunit
 
 let testFile fileName =
-  let source =
-    IO.File.ReadAllText(
-      IO.Path.Combine(testsDir.Value, "src", fileName + ".milone")
-    )
-  let content =
-    let cir = Program.toCir source
-    CPrinting.cprintRun (fun acc -> CPrinting.cprintDecls acc cir)
-  let target =
-    IO.File.WriteAllText(
-      IO.Path.Combine(testsDir.Value, "c", fileName + ".c"),
-      content
-    )
-  ()
+  async {
+    let! source =
+      IO.File.ReadAllTextAsync(
+        IO.Path.Combine(testsDir.Value, "src", fileName + ".milone")
+      ) |> Async.AwaitTask
+    let content =
+      let cir = Program.toCir source
+      CPrinting.cprintRun (fun acc -> CPrinting.cprintDecls acc cir)
+    do!
+      IO.File.WriteAllTextAsync(
+        IO.Path.Combine(testsDir.Value, "c", fileName + ".c"),
+        content
+      ) |> Async.AwaitTask
+  }
+
+let collectTestTargets () =
+  seq {
+    let files = IO.Directory.GetFiles(Path.Combine(testsDir.Value, "src"), "*.milone")
+    for file in files do
+      let name = IO.Path.GetFileNameWithoutExtension(file)
+      yield [|name :> obj|]
+  }
 
 [<Theory>]
-[<InlineData("zero")>]
-[<InlineData("add")>]
-[<InlineData("arith")>]
-[<InlineData("logic")>]
-[<InlineData("anno")>]
-[<InlineData("hello")>]
-[<InlineData("printfn")>]
-[<InlineData("var_1")>]
-[<InlineData("var_str")>]
-[<InlineData("shadowing")>]
-[<InlineData("if_else")>]
-[<InlineData("if_else_complex")>]
-[<InlineData("factorial")>]
-[<InlineData("fizz_buzz")>]
+[<MemberData("collectTestTargets")>]
 let integrationTests (name: string) =
   testFile name
