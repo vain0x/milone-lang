e9cf5aa6e8f79744e4ee2ae69d8f52fbeab7c35e
vain0x <vainzerox@gmail.com>
2019-05-21 09:03:14 +0900

    feat: Combine open modules into single AST

--- boot/MiloneLang/Program.fs
+++ boot/MiloneLang/Program.fs
@@ -5,10 +5,34 @@ module MiloneLang.Program
     | Verbose
     | Silent
 
-  let readProjectSource (projectDir: string) =
+  let parseModuleFile (projectDir: string) (moduleName: string) =
+    let filePath = IO.Path.Combine(projectDir, sprintf "%s.milone" moduleName)
+    let source = IO.File.ReadAllText(filePath)
+    let tokens = Lexing.tokenize source
+    let ast = Parsing.parse tokens
+    ast
+
+  let parseProjectModules (projectDir: string) =
     let projectName = IO.Path.GetFileName(projectDir)
-    let mainPath = IO.Path.Combine(projectDir, sprintf "%s.milone" projectName)
-    IO.File.ReadAllText(mainPath)
+
+    let rec go moduleAcc moduleMap moduleName =
+      if moduleMap |> Map.containsKey moduleName then
+        moduleAcc, moduleMap
+      else
+        let moduleAst = parseModuleFile projectDir moduleName
+        let dependencies = Typing.findOpenModules projectName moduleAst
+        let moduleMap = moduleMap |> Map.add moduleName moduleAst
+        let moduleAcc, moduleMap =
+          List.fold (fun (moduleAcc, moduleMap) dep ->
+            let moduleAcc, moduleMap = go moduleAcc moduleMap dep
+            moduleAcc, moduleMap
+          ) ([], moduleMap) dependencies
+        moduleAst :: moduleAcc, moduleMap
+
+    let moduleAcc, _ = go [] Map.empty projectName
+    let modules = moduleAcc |> List.rev
+
+    MiloneLang.Helpers.hxAndThen modules (0, 0)
 
   let toCir verbosity (projectDir: string): CDecl list * bool =
     let log label obj =
@@ -18,11 +42,11 @@ module MiloneLang.Program
       | Verbosity.Silent ->
         ()
 
-    let source = readProjectSource projectDir
-    let tokens = Lexing.tokenize source
-    log "tokens" tokens
-    let ast = Parsing.parse tokens
-    log "ast" ast
+    let ast = parseProjectModules projectDir
+    // let tokens = Lexing.tokenize source
+    // log "tokens" tokens
+    // let ast = Parsing.parse tokens
+    // log "ast" ast
     let desugared = Desugaring.desugar ast
     log "desugared" ast
     let typedAst, tyCtx = Typing.infer desugared
---
+++ boot/tests/features/modules/modules.c
@@ -0,0 +1,7 @@
+int main();
+
+int main() {
+    printf("Hello\n");
+    int call_ = 0;
+    return 0;
+}
