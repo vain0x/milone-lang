34013532a2c4badd19930195d8866bfd7f00f975
vain0x <vainzerox@gmail.com>
2019-09-06 20:17:09 +0900

    fix: Fix a test
    Payload types need wrapping with a parenthesis if function type.

--- boot/tests/features/union_complex/union_complex.fs
+++ boot/tests/features/union_complex/union_complex.fs
@@ -11,11 +11,11 @@ type Node =
 // Unions that mutually recursive via lists, tuples and functions.
 type First =
   | First
-    of (First * Second) list -> First list * Second list
+    of ((First * Second) list -> First list * Second list)
 
 type Second =
   | Second
-    of (First * Second) list -> First list * Second list
+    of ((First * Second) list -> First list * Second list)
 
 let main _ =
   match Node [Node [Leaf 1; Leaf 2]; Node []; Node [Leaf 3]] with
