3d85bc4d46e9287772b0a5d726c413fa8fb4d053
vain0x <vainzerox@gmail.com>
2019-09-12 23:22:53 +0900

    refactor: Solve ABC140 A (Competitive)

---
+++ boot/tests/examples/Competitive/Competitive.fs
@@ -0,0 +1,8 @@
+module rec Competitive
+
+open Competitive.Helpers
+open Competitive.ABC140A
+
+let main _ =
+  abc140aTest ()
+  0
---
+++ boot/tests/examples/Competitive/Helpers.fs
@@ -0,0 +1,4 @@
+module rec Competitive.Helpers
+
+let scanInt (): int =
+  (__nativeFun "scan_int" 1) 0
