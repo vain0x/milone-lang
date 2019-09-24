47b4294cb3a9edbc25c40f2b59b4af8b4088fbf4
vain0x <vainzerox@gmail.com>
2019-09-24 23:33:05 +0900

    refactor: Update a test for closure conversion bug

--- boot/tests/features/let_fun_closure/let_fun_closure.fs
+++ boot/tests/features/let_fun_closure/let_fun_closure.fs
@@ -1,4 +1,25 @@
+let basicCase () =
+  let step = 3
+  let sub x = x - step
+  assert (sub 8 = 5)
+
+let transitiveCase () =
+  let step = 1
+  let inc x = x + step
+  let incTwice x = x |> inc |> inc
+  assert (incTwice 0 = 2)
+
+let recursiveCase () =
+  let step = 1
+  let inc x = x + step
+
+  let rec go n =
+    n >= 10 || n |> inc |> go
+
+  assert (go 0)
+
 let main _ =
-  let y = 3
-  let sub x = x - y
-  sub 8 - 5
+  basicCase ()
+  transitiveCase ()
+  recursiveCase ()
+  0
