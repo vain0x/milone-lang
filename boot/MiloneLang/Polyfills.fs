15da5a0e7b7cd6a20f00910f8d5659dec2dd2625
vain0x <vainzerox@gmail.com>
2019-10-06 16:52:48 +0900

    feat: Add modules for self-compile

---
+++ boot/MiloneLang/Polyfills.fs
@@ -0,0 +1,19 @@
+/// Defines F# primitives for milone-lang.
+[<AutoOpen>]
+module rec MiloneLang.Polyfills
+
+let failwith (str: string) =
+  printfn "FATAL ERROR: %s" str
+  exit 1
+
+let failwithf (str: string) =
+  printfn "FATAL ERROR: %s" str
+  exit 1
+
+let sprintf (fmt: string) =
+  printfn "%s" fmt
+  exit 1
+
+let id x = x
+
+let fst (x, _) = x
