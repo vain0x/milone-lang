465dcd6f954bacff256651b71c66c2317b353b82
vain0x <vainzerox@gmail.com>
2020-10-26 23:04:42 +0900

    refactor: Reorganization of functions

--- boot/MiloneLang/KirGen.fs
+++ boot/MiloneLang/KirGen.fs
@@ -128,8 +128,24 @@ let private kgPat (pat: HPat) resolve reject ctx: PNode =
   | HAnnoPat _ -> failwithf "NEVER: Anno pattern is resolved in type inference. %A" pat
 
 // -----------------------------------------------
+// KirGenCtx
+// -----------------------------------------------
+
+let private newVar hint ty loc ctx = failwith ""
+
+let private newFun hint ty loc ctx = failwith ""
+
+let private newFunSerial loc ctx = failwith ""
+
+let private addFun funSerial funDef funBinding ctx = failwith ""
+
+// -----------------------------------------------
 // Emission helpers
 // -----------------------------------------------
+
+let private abortNode loc ctx =
+  let code = KLitTerm(IntLit 1, loc)
+  KPrimNode(KExitPrim, [ code ], [], [], loc), ctx
 
 /// Converts to basic 2-arity prim node.
 let private basicPrimNode2 hint prim l r ty loc hole ctx =
@@ -142,7 +158,7 @@ let private basicPrimNode2 hint prim l r ty loc hole ctx =
             KPrimNode(prim, [ l; r ], [ result ], [ cont ], loc), ctx))
 
 // -----------------------------------------------
-// Call non-primitive, semi
+// Non-prim basic expr
 // -----------------------------------------------
 
 let private kgSemiExpr itself args hole ctx =
@@ -158,7 +174,6 @@ let private kgSemiExpr itself args hole ctx =
 
   go args hole ctx
 
-// call to something non-prim, non-variant. typically a function.
 let private kgCallFunExpr funSerial funTy funLoc args ty loc hole ctx =
   ctx
   |> kgExprList args (fun args ctx ->
@@ -166,6 +181,7 @@ let private kgCallFunExpr funSerial funTy funLoc args ty loc hole ctx =
        let cont, ctx = hole (KVarTerm(result, ty, loc)) ctx
        KPrimNode(KCallProcPrim, KFunTerm(funSerial, funTy, funLoc) :: args, [ result ], [ cont ], loc), ctx)
 
+/// Converts an expr calling to variant, i.e., construction of a variant with payload.
 let private kgCallVariantExpr variantSerial variantTy variantLoc args ty loc hole ctx =
   let variantIdent =
     ctx |> findVarDef variantSerial |> varDefToIdent
@@ -179,8 +195,7 @@ let private kgCallVariantExpr variantSerial variantTy variantLoc args ty loc hol
        let cont, ctx = hole (KVarTerm(result, ty, loc)) ctx
        KPrimNode(KVariantPrim, variantTerm :: args, [ result ], [ cont ], loc), ctx)
 
-// call to something non-prim, non-variant, non-fun. i.e. a closure.
-let private kgCallClosureExpr expr callee args ty loc hole ctx =
+let private kgCallClosureExpr callee args ty loc hole ctx =
   ctx
   |> kgExpr callee (fun callee ctx ->
        ctx
@@ -189,9 +204,26 @@ let private kgCallClosureExpr expr callee args ty loc hole ctx =
             let cont, ctx = hole (KVarTerm(result, ty, loc)) ctx
             KPrimNode(KCallClosurePrim, callee :: args, [ result ], [ cont ], loc), ctx))
 
+let private kgTupleExpr args ty loc hole ctx =
+  ctx
+  |> kgExprList args (fun args ctx ->
+       let result, ctx = ctx |> newVar "tuple" ty loc
+       let cont, ctx = hole (KVarTerm(result, ty, loc)) ctx
+       KPrimNode(KTuplePrim, args, [ result ], [ cont ], loc), ctx)
+
+let private kgClosureExpr funSerial funTy funLoc env ty loc hole ctx =
+  ctx
+  |> kgExpr env (fun env ctx ->
+       let funTerm = KFunTerm(funSerial, funTy, funLoc)
+       let result, ctx = ctx |> newVar "closure" ty loc
+       let cont, ctx = hole (KVarTerm(result, ty, loc)) ctx
+       KPrimNode(KClosurePrim, [ funTerm; env ], [ result ], [ cont ], loc), ctx)
+
 // -----------------------------------------------
-// Primitive dispatch
+// Prims
 // -----------------------------------------------
+
+// Handling prims individually, which tends to code verbose yet easy to maintain.
 
 // prim expr, not appear as callee
 let private kgPrimExpr expr prim ty loc hole ctx: KNode * kgx =
@@ -228,7 +260,7 @@ let private kgCallAddExpr itself _primTy args ty loc hole ctx =
 
   | _ -> unreachable itself
 
-// Since exit doesn't return, hole is not used.
+/// Converts call to exit. Since exit doesn't return, hole is discarded.
 let private kgCallExitExpr itself args primLoc ctx =
   match args with
   | [ arg ] ->
@@ -237,105 +269,17 @@ let private kgCallExitExpr itself args primLoc ctx =
 
   | _ -> unreachable itself
 
-// call to non-special prim
-let private kgCallOtherPrimExpr hint prim args ty loc hole ctx =
+/// Converts call to regular (not special) prim.
+let private kgCallRegularPrimExpr hint prim args ty loc hole ctx =
   ctx
   |> kgExprList args (fun args ctx ->
        let result, ctx = ctx |> newVar hint ty loc
        let cont, ctx = hole (KVarTerm(result, ty, loc)) ctx
        KPrimNode(prim, args, [ result ], [ cont ], loc), ctx)
 
-let private kgInfExpr itself infOp args ty loc hole ctx: KNode * kgx =
-  match infOp with
-  | InfOp.Semi -> kgSemiExpr itself args hole ctx
-
-  | InfOp.CallProc ->
-      let callee, args =
-        match args with
-        | callee :: args -> callee, args
-        | [] -> failwithf "NEVER: CallProc args must begin with callee. %A" itself
-
-      match callee with
-      | HPrimExpr (prim, primTy, primLoc) ->
-          let other hint prim =
-            kgCallOtherPrimExpr hint prim args ty primLoc hole ctx
-
-          match prim with
-          | HPrim.Add -> kgCallAddExpr itself primTy args ty primLoc hole ctx
-          | HPrim.Sub -> other "sub" KSubPrim
-          | HPrim.Mul -> other "mul" KMulPrim
-          | HPrim.Div -> other "div" KDivPrim
-          | HPrim.Mod -> other "mod" KModPrim
-          | HPrim.Eq -> other "equal" KEqualPrim
-          | HPrim.Lt -> other "less" KLessPrim
-          | HPrim.Nil -> unreachable itself
-          | HPrim.Cons -> other "cons" KConsPrim
-          | HPrim.OptionNone -> unreachable itself
-          | HPrim.OptionSome -> other "some" KSomePrim
-          | HPrim.Index -> other "index" KStrIndexPrim
-          | HPrim.Not -> other "not" KNotPrim
-          | HPrim.Exit -> kgCallExitExpr itself args primLoc ctx
-          | HPrim.Assert -> other "assert" KAssertPrim
-          | HPrim.Box -> other "box" KBoxPrim
-          | HPrim.Unbox -> other "unbox" KUnboxPrim
-          | HPrim.Printfn -> other "printfn" KPrintfnPrim
-          | HPrim.StrLength -> other "str_length" KStrLengthPrim
-          | HPrim.StrGetSlice -> other "str_get_slice" KStrGetSlicePrim
-          | HPrim.Char -> other "char" KCharPrim
-          | HPrim.Int -> other "int" KIntPrim
-          | HPrim.String -> other "string" KStringPrim
-          | HPrim.InRegion -> other "in_region" KInRegionPrim
-          | HPrim.NativeFun (ident, arity) -> other ident (KNativeFunPrim(ident, arity))
-
-      | HRefExpr (varSerial, refTy, refLoc) ->
-          match ctx |> findVarDef varSerial with
-          | VarDef _ -> failwithf "NEVER: CallClosure should be used. %A" itself
-          | FunDef _ -> kgCallFunExpr varSerial refTy refLoc args ty loc hole ctx
-          | VariantDef _ -> kgCallVariantExpr varSerial refTy refLoc args ty loc hole ctx
-
-      | _ -> failwithf "NEVER: CallClosure should be used. %A" itself
-
-  | InfOp.CallClosure ->
-      match args with
-      | callee :: args -> kgCallClosureExpr itself callee args ty loc hole ctx
-      | [] -> failwithf "NEVER: CallClosure args must begin with callee. %A" itself
-
-  | InfOp.Tuple ->
-      match args, ty with
-      | [], _ -> hole (KUnitTerm loc) ctx
-
-      | _, AppTy (TupleTyCtor, itemTys) ->
-          // mirifyExprTuple ctx args itemTys loc
-          failwith ""
-
-      | _ -> failwithf "NEVER: tuple ctor must be of tuple type. %A" itself
-
-  | InfOp.Closure ->
-      match args with
-      | [ HRefExpr (funSerial, _, _); env ] ->
-          // mirifyExprInfClosure ctx funSerial env ty loc
-          failwith ""
-
-      | _ -> failwithf "NEVER: bad use of Closure prim. %A" itself
-
-  | InfOp.App -> failwithf "NEVER: InfOp.App is resolved in uneta. %A" itself
-  | InfOp.Anno -> failwithf "NEVER: InfOp.Anno is resolved in type inference: %A" itself
-
 // -----------------------------------------------
-// match, let-val, let-fun
+// if, match
 // -----------------------------------------------
-
-let private newVar hint ty loc ctx = failwith ""
-
-let private newFun hint ty loc ctx = failwith ""
-
-let private newFunSerial loc ctx = failwith ""
-
-let private addFun funSerial funDef funBinding ctx = failwith ""
-
-let private kgAbort loc ctx =
-  let code = KLitTerm(IntLit 1, loc)
-  KPrimNode(KExitPrim, [ code ], [], [], loc), ctx
 
 let private kgIfNode _ = failwith ""
 
@@ -381,7 +325,7 @@ let private kgMatchExpr expr cond arms targetTy loc hole ctx: KNode * kgx =
     match arms with
     | [] ->
         // Non-exhaustive error.
-        kgAbort loc ctx
+        abortNode loc ctx
 
     | (pat, guard, body) :: arms ->
         let loc = patToLoc pat
@@ -434,6 +378,10 @@ let private kgMatchExpr expr cond arms targetTy loc hole ctx: KNode * kgx =
 
   node, ctx
 
+// -----------------------------------------------
+// let-val, let-fun
+// -----------------------------------------------
+
 let private kgLetValExpr expr pat init next nextTy loc hole ctx: KNode * kgx = failwith ""
 
 let private kgLetFunExpr expr funSerial isMainFun args body next nextTy loc hole ctx: KNode * kgx = failwith ""
@@ -441,6 +389,75 @@ let private kgLetFunExpr expr funSerial isMainFun args body next nextTy loc hole
 // -----------------------------------------------
 // kgExpr
 // -----------------------------------------------
+
+// Controller of the pass.
+// Decompose an HIR expr and dispatch to one of funs to generate KIR node, defined above.
+
+let private kgInfExpr itself infOp args ty loc hole ctx: KNode * kgx =
+  match infOp with
+  | InfOp.Semi -> kgSemiExpr itself args hole ctx
+
+  | InfOp.CallProc ->
+      let callee, args =
+        match args with
+        | callee :: args -> callee, args
+        | [] -> failwithf "NEVER: CallProc args must begin with callee. %A" itself
+
+      match callee with
+      | HPrimExpr (prim, primTy, primLoc) ->
+          let regular hint prim =
+            kgCallRegularPrimExpr hint prim args ty primLoc hole ctx
+
+          match prim with
+          | HPrim.Add -> kgCallAddExpr itself primTy args ty primLoc hole ctx
+          | HPrim.Sub -> regular "sub" KSubPrim
+          | HPrim.Mul -> regular "mul" KMulPrim
+          | HPrim.Div -> regular "div" KDivPrim
+          | HPrim.Mod -> regular "mod" KModPrim
+          | HPrim.Eq -> regular "equal" KEqualPrim
+          | HPrim.Lt -> regular "less" KLessPrim
+          | HPrim.Nil -> unreachable itself
+          | HPrim.Cons -> regular "cons" KConsPrim
+          | HPrim.OptionNone -> unreachable itself
+          | HPrim.OptionSome -> regular "some" KSomePrim
+          | HPrim.Index -> regular "index" KStrIndexPrim
+          | HPrim.Not -> regular "not" KNotPrim
+          | HPrim.Exit -> kgCallExitExpr itself args primLoc ctx
+          | HPrim.Assert -> regular "assert" KAssertPrim
+          | HPrim.Box -> regular "box" KBoxPrim
+          | HPrim.Unbox -> regular "unbox" KUnboxPrim
+          | HPrim.Printfn -> regular "printfn" KPrintfnPrim
+          | HPrim.StrLength -> regular "str_length" KStrLengthPrim
+          | HPrim.StrGetSlice -> regular "str_get_slice" KStrGetSlicePrim
+          | HPrim.Char -> regular "char" KCharPrim
+          | HPrim.Int -> regular "int" KIntPrim
+          | HPrim.String -> regular "string" KStringPrim
+          | HPrim.InRegion -> regular "in_region" KInRegionPrim
+          | HPrim.NativeFun (ident, arity) -> regular ident (KNativeFunPrim(ident, arity))
+
+      | HRefExpr (varSerial, refTy, refLoc) ->
+          match ctx |> findVarDef varSerial with
+          | VarDef _ -> failwithf "NEVER: CallClosure should be used. %A" itself
+          | FunDef _ -> kgCallFunExpr varSerial refTy refLoc args ty loc hole ctx
+          | VariantDef _ -> kgCallVariantExpr varSerial refTy refLoc args ty loc hole ctx
+
+      | _ -> failwithf "NEVER: CallClosure should be used. %A" itself
+
+  | InfOp.CallClosure ->
+      match args with
+      | callee :: args -> kgCallClosureExpr callee args ty loc hole ctx
+      | [] -> failwithf "NEVER: CallClosure args must begin with callee. %A" itself
+
+  | InfOp.Tuple -> kgTupleExpr args ty loc hole ctx
+
+  | InfOp.Closure ->
+      match args with
+      | [ HRefExpr (funSerial, funTy, funLoc); env ] -> kgClosureExpr funSerial funTy funLoc env ty loc hole ctx
+
+      | _ -> failwithf "NEVER: bad use of Closure prim. %A" itself
+
+  | InfOp.App -> failwithf "NEVER: InfOp.App is resolved in uneta. %A" itself
+  | InfOp.Anno -> failwithf "NEVER: InfOp.Anno is resolved in type inference: %A" itself
 
 /// Evaluates an expression and fills a hole with the result term.
 let private kgExpr (expr: HExpr) (hole: KTerm -> kgx -> KNode * kgx) (ctx: kgx): KNode * kgx =
@@ -478,7 +495,7 @@ let private kgExpr (expr: HExpr) (hole: KTerm -> kgx -> KNode * kgx) (ctx: kgx):
   | HModuleExpr _ -> failwithf "NEVER: module is resolved in name res. %A" expr
 
 /// Converts a list of expressions sequentially
-/// and fills a hole with a list of their results.
+/// and fills a hole with the list of their results.
 let private kgExprList args hole ctx =
   match args with
   | [] -> hole [] ctx
