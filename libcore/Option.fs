d1ed7331f5245b25adf6e040bb04ddc90c230235
vain0x <vainzerox@gmail.com>
2020-12-01 23:06:00 +0900

    feat: Add more functions to Option

--- libcore/Option.fs
+++ libcore/Option.fs
@@ -1,21 +1,41 @@
 module rec MiloneCore.Option
 
-let isNone option =
-  match option with
+let isNone opt =
+  match opt with
   | None -> true
   | Some _ -> false
 
-let defaultValue alt option =
-  match option with
+let isSome opt =
+  match opt with
+  | None -> false
+  | Some _ -> true
+
+let defaultValue alt opt =
+  match opt with
   | Some x -> x
   | None -> alt
 
-let map f option =
-  match option with
+let defaultWith alt opt =
+  match opt with
+  | Some x -> x
+  | None -> alt ()
+
+let map f opt =
+  match opt with
   | Some x -> Some(f x)
   | None -> None
 
-let forall pred option =
-  match option with
+let bind f opt =
+  match opt with
+  | Some x -> f x
+  | None -> None
+
+let forall pred opt =
+  match opt with
   | Some x -> pred x
   | None -> true
+
+let exists pred opt =
+  match opt with
+  | Some x -> pred x
+  | None -> false
