be842950b1e7aab0e098eb08f0b513f33b4b601b
vain0x <vainzerox@gmail.com>
2020-12-30 21:32:10 +0900

    refactor: Add test about ty vars in function body

--- tests/features/ty_var/ty_var.fs
+++ tests/features/ty_var/ty_var.fs
@@ -11,9 +11,17 @@ let useStringIndexWithoutAnnotation x =
   // This passes the type check without explicit x: string annotation.
   x.[0]
 
+let useTyVarInBody (x: 'T) =
+  let xs: 'T list = []
+  x, xs
+
 let main _ =
   assert (pair (0, 0) = 2)
   assert (pair ("", "") = 2)
 
   assert (useStringIndexWithoutAnnotation "a" = 'a')
+
+  match useTyVarInBody "b" with
+  | _, s :: _ -> assert (s.[0] = 'b')
+  | _ -> ()
   0
