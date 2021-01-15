1648bec5ac28625b9cb54bc3e642bdcba09827a4
vain0x <vainzerox@gmail.com>
2021-01-16 00:37:43 +0900

    refactor: Reproduce an issue
    When types and modules of the same name are imported,
    the type name is shadowed unexpectedly.

---
+++ tests/features/module_along_with_type/sub.fs
@@ -0,0 +1,16 @@
+module rec module_along_with_type.sub
+
+type Pos = Pos of int * int
+
+module rec Pos =
+  let eol = Pos(1, 0)
+
+  let add l r: Pos =
+    let (Pos (ly, lx)) = l
+    let (Pos (ry, rx)) = r
+    Pos(ly + ry, lx + rx)
+
+// Extension.
+module rec Pos =
+  let toString (Pos (row, column)) =
+    string (row + 1) + ":" + string (column + 1)
