a773620c8418ca61d6aefdce1e8bfaf3a05adcad
vain0x <vainzerox@gmail.com>
2021-01-25 22:38:54 +0900

    feat: Counter app
    Memory is still leaked.

--- examples/AppGtk/app_gtk.c
+++ examples/AppGtk/app_gtk.c
@@ -15,6 +15,10 @@ struct ObjectObjectObjectFun2 {
     void const *env;
 };
 
+void str_error(struct String s) {
+    fprintf(stderr, "error: %s\n", str_to_c_str(s));
+}
+
 // -----------------------------------------------
 // GTK
 // -----------------------------------------------
@@ -29,17 +33,88 @@ static ViewFn s_view;
 
 static int s_msg = 0;
 
-static GtkWidget *s_label;
+static GtkWidget *s_grid;
+
+static void const *s_ui;
 
 static void update_view(void) {
-    struct String value =
-        *(struct String const *)s_view.fun(s_view.env, s_state);
-    gtk_label_set_text(GTK_LABEL(s_label), str_to_c_str(value));
+    fprintf(stderr, "trace: update_view\n");
+    s_view.fun(s_view.env, s_state);
+    // struct String value =
+    //     *(struct String const *)s_view.fun(s_view.env, s_state);
+    // gtk_label_set_text(GTK_LABEL(s_label), str_to_c_str(value));
 }
 
-static void on_button_clicked(GtkWidget *button, gpointer _data) {
-    s_state = s_update.fun(s_update.env, s_state, &s_msg);
+static void on_button_clicked(GtkWidget *button, gpointer data) {
+    fprintf(stderr, "trace: on_button_clicked data=%p\n", data);
+    int msg = (int)(uintptr_t)data;
+
+    s_state = s_update.fun(s_update.env, s_state, &msg);
     update_view();
+}
+
+void milone_gtk_set_view(void const *ui) {
+    fprintf(stderr, "trace: set_view %p -> %p\n", s_ui, ui);
+    s_ui = ui;
+}
+
+void const *milone_gtk_get_view(void) {
+    fprintf(stderr, "trace: get_view %p\n", s_ui);
+    return s_ui;
+}
+
+void milone_gtk_apply_diff(struct String diff_string) {
+    GtkWidget *grid = s_grid;
+
+    char *diff = strdup(str_to_c_str(diff_string));
+    char *tok = NULL;
+    char const *delim = " \r\n";
+
+    while (true) {
+        char *s = strtok_r(diff, delim, &tok);
+        if (s == NULL) {
+            break;
+        }
+
+        // Non-first call to strtok_r should pass NULL as first argument.
+        diff = NULL;
+
+        fprintf(stderr, "tok: '%s' (%ld)\n", s, strlen(s));
+
+        if (strcmp(s, "ADD_LABEL") == 0) {
+            int index = str_to_int(str_borrow(strtok_r(NULL, delim, &tok)));
+            char const *text = strtok_r(NULL, delim, &tok);
+            fprintf(stderr, "trace: add_label index=%d text=%s\n", index, text);
+
+            GtkWidget *label = gtk_label_new(text);
+            gtk_grid_attach(GTK_GRID(grid), label, 0, index, 1, 1);
+            continue;
+        }
+
+        if (strcmp(s, "ADD_BUTTON") == 0) {
+            int index = str_to_int(str_borrow(strtok_r(NULL, delim, &tok)));
+            int msg = str_to_int(str_borrow(strtok_r(NULL, delim, &tok)));
+            char const *text = strtok_r(NULL, delim, &tok);
+
+            GtkWidget *button = gtk_button_new_with_label(text);
+            g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), (gpointer)(uintptr_t)msg);
+            gtk_grid_attach(GTK_GRID(grid), button, 0, index, 1, 1);
+            continue;
+        }
+
+        if (strcmp(s, "SET_LABEL") == 0) {
+            int index = str_to_int(str_borrow(strtok_r(NULL, delim, &tok)));
+            char const *text = strtok_r(NULL, delim, &tok);
+            fprintf(stderr, "trace: set_label index=%d text=%s\n", index, text);
+
+            GtkWidget *label = gtk_grid_get_child_at(GTK_GRID(grid), 0, index);
+            gtk_label_set_text(GTK_LABEL(label), text);
+            continue;
+        }
+
+        fprintf(stderr, "unknown msg: '%s' (%ld)\n", s, strlen(s));
+        break;
+    }
 }
 
 static void activate(GtkApplication *app, gpointer _data) {
@@ -49,19 +124,21 @@ static void activate(GtkApplication *app, gpointer _data) {
 
     GtkWidget *grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(window), grid);
+    s_grid = grid;
 
     // GtkWidget *button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
     // gtk_container_add(GTK_CONTAINER(window), button_box);
 
-    GtkWidget *button = gtk_button_new_with_label("Hello World");
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
-    // gtk_container_add(GTK_CONTAINER(button_box), button);
-    gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
+    // GtkWidget *button = gtk_button_new_with_label("Hello World");
+    // g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    // // gtk_container_add(GTK_CONTAINER(button_box), button);
+    // gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
 
-    GtkWidget *label = gtk_label_new("0");
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
-    s_label = label;
+    // GtkWidget *label = gtk_label_new("0");
+    // gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
+    // s_label = label;
 
+    update_view();
     gtk_widget_show_all(window);
 }
 
