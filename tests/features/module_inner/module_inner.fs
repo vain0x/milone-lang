15f80ca0274e383b025a7f22e89c83a8604cbe21
vain0x <vainzerox@gmail.com>
2021-03-13 17:45:01 +0900

    refactor: Add test that path expression can denote to inner module in another file

--- tests/features/module_inner/module_inner.fs
+++ tests/features/module_inner/module_inner.fs
@@ -2,9 +2,15 @@ module rec module_inner.Program
 
 // Inner module statements.
 
+module sub = module_inner.sub
+
 module rec Inner =
   let f x = x + 1
 
 let main _ =
+  // Inner module in the same file.
   assert (Inner.f 2 = 3)
+
+  // Inner module in another file.
+  assert (sub.Inner.decrement 3 = 2)
   0
