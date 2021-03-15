713b5be531d175ecd3ad307de8e7bffb6a2fae24
vain0x <vainzerox@gmail.com>
2021-03-15 20:52:15 +0900

    wip!  [skip ci]

--- examples/AppGtk/app_gtk.c
+++ examples/AppGtk/app_gtk.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <gtk/gtk.h>
 #include <milone.h>
 
@@ -23,6 +24,14 @@ void str_error(struct String s) {
 // MyState
 // -----------------------------------------------
 
+static bool same_ptr(void const *l, void const *r) {
+    if (l == r) {
+        fprintf(stderr, "same: %p\n", l);
+        return true;
+    }
+    return false;
+}
+
 struct IntOption {
     bool some;
     int value;
@@ -30,83 +39,104 @@ struct IntOption {
 
 struct IntOptionList {
     struct IntOption head;
-    struct IntOptionList const *tail;
+    struct IntOptionList *tail;
 };
 
 struct MyState_ {
     int t0;
-    struct IntOptionList const *t1;
+    struct IntOptionList *t1;
+    struct IntOptionList *t2;
 };
 
-struct PtrChain {
-    void const *ptr;
-    struct PtrChain *prev;
-    struct PtrChain *next;
-};
+// struct PtrChain {
+//     void const *ptr;
+//     struct PtrChain *prev;
+//     struct PtrChain *next;
+// };
 
-static struct PtrChain *s_chain = NULL;
+// static struct PtrChain *s_chain = NULL;
 
-static bool p_find(void const *p, struct PtrChain **out_chain) {
-    assert(p != NULL);
+// static bool p_find(void const *p, struct PtrChain **out_chain) {
+//     assert(p != NULL);
 
-    for (struct PtrChain *c = s_chain; c != NULL; c = c->next) {
-        if (c->ptr == p) {
-            *out_chain = c;
-            return true;
-        }
-    }
+//     for (struct PtrChain *c = s_chain; c != NULL; c = c->next) {
+//         if (c->ptr == p) {
+//             *out_chain = c;
+//             return true;
+//         }
+//     }
 
-    *out_chain = NULL;
-    return false;
+//     *out_chain = NULL;
+//     return false;
+// }
+
+// static bool is_promoted(void const *p) {
+//     struct PtrChain *c;
+//     return p != NULL && p_find(p, &c);
+// }
+
+// static void p_link(void *p) {
+//     assert(p != NULL);
+//     assert(!is_promoted(p));
+
+//     struct PtrChain *c = malloc(sizeof(struct PtrChain));
+//     *c = (struct PtrChain){.ptr = p, .prev = NULL, .next = s_chain};
+//     s_chain = c;
+// }
+
+// static void p_unlink(void *p) {
+//     if (p == NULL) {
+//         return;
+//     }
+
+//     struct PtrChain *c;
+//     if (!p_find(p, &c)) {
+//         return;
+//     }
+
+//     // Fix links.
+//     if (c->prev != NULL) {
+//         assert(c->prev->next == c);
+//         c->prev->next = c->next;
+//     }
+//     if (c->next != NULL) {
+//         assert(c->next->prev == c);
+//         c->next->prev = c->prev;
+//     }
+//     if (c == s_chain) {
+//         s_chain = c->next;
+//     }
+
+//     free(c);
+// }
+
+static void promote_int_option_list(struct IntOptionList **old_ptr,
+                                    struct IntOptionList *const *new_ptr);
+
+static struct IntOptionList *allocate_cons(struct IntOption head,
+                                           struct IntOptionList *tail) {
+    struct IntOptionList *cons = malloc(sizeof(struct IntOptionList));
+    cons->head = head;
+    cons->tail = NULL; // set default
+    fprintf(stderr, "new cons: %p\n", cons);
+
+    // Can't do `cons->tail = tail` because tail is not promoted.
+    promote_int_option_list(&cons->tail, &tail);
+    return cons;
 }
 
-static bool is_promoted(void const *p) {
-    struct PtrChain *c;
-    return p != NULL && p_find(p, &c);
+static void free_cons(struct IntOptionList *cons) {
+    fprintf(stderr, "free cons: %p\n", cons);
+    free(cons);
 }
 
-static void p_link(void *p) {
-    assert(p != NULL);
-    assert(!is_promoted(p));
-
-    struct PtrChain *c = malloc(sizeof(struct PtrChain));
-    *c = (struct PtrChain){.ptr = p, .prev = NULL, .next = s_chain};
-    s_chain = c;
-}
-
-static void p_unlink(void *p) {
-    if (p == NULL) {
+static void promote_int_option_list(struct IntOptionList **old_ptr,
+                                    struct IntOptionList *const *new_ptr) {
+    if (same_ptr(old_ptr, new_ptr)) {
         return;
     }
 
-    struct PtrChain *c;
-    if (!p_find(p, &c)) {
-        return;
-    }
-
-    // Fix links.
-    if (c->prev != NULL) {
-        assert(c->prev->next == c);
-        c->prev->next = c->next;
-    }
-    if (c->next != NULL) {
-        assert(c->next->prev == c);
-        c->next->prev = c->prev;
-    }
-    if (c == s_chain) {
-        s_chain = c->next;
-    }
-
-    free(c);
-}
-
-void promote_int_option_list(struct IntOptionList const **old_ptr,
-                             struct IntOptionList const **new_ptr) {
-    if (old_ptr == new_ptr) {
-        return;
-    }
-
-    // free or (_, nil)
+    // free or (_, nil) | ((some x, some y) when x <> y)
     if (new_ptr == NULL || *new_ptr == NULL) {
         if (*old_ptr != NULL) {
             // free head: pass.
@@ -115,7 +145,8 @@ void promote_int_option_list(struct IntOptionList const **old_ptr,
             promote_int_option_list(&(*old_ptr)->tail, NULL);
 
             // free cons
-            free(*old_ptr);
+            free_cons(*old_ptr);
+            *old_ptr = NULL;
         }
         return;
     }
@@ -123,13 +154,8 @@ void promote_int_option_list(struct IntOptionList const **old_ptr,
     // nil, cons
     if (*old_ptr == NULL) {
         assert(*new_ptr != NULL);
-
-        // allocate cons
-        struct IntOptionList *cons = malloc(sizeof(struct IntOptionList));
-        cons->head = (*new_ptr)->head;
-        cons->tail = NULL; // set default
-        promote_int_option_list(&cons->tail, &(*new_ptr)->tail);
-        *old_ptr = cons;
+        *old_ptr = allocate_cons((*new_ptr)->head, (*new_ptr)->tail);
+        return;
     }
 
     // cons, cons
@@ -137,15 +163,16 @@ void promote_int_option_list(struct IntOptionList const **old_ptr,
     assert(*new_ptr != NULL);
 
     // promote head (just copy);
-    (*(struct IntOptionList **)old_ptr)->head = (*new_ptr)->head;
+    (*old_ptr)->head = (*new_ptr)->head;
 
     // promote tail
     promote_int_option_list(&(*old_ptr)->tail, &(*new_ptr)->tail);
 }
 
-void promote_my_state(struct MyState_ *old_ptr, struct MyState_ *new_ptr) {
-    if (old_ptr == new_ptr) {
-        return old_ptr;
+static void promote_my_state(struct MyState_ *old_ptr,
+                             struct MyState_ const *new_ptr) {
+    if (same_ptr(old_ptr, new_ptr)) {
+        return;
     }
 
     // free each field
@@ -157,6 +184,7 @@ void promote_my_state(struct MyState_ *old_ptr, struct MyState_ *new_ptr) {
     // promote each field
     old_ptr->t0 = new_ptr->t0;
     promote_int_option_list(&old_ptr->t1, &new_ptr->t1);
+    promote_int_option_list(&old_ptr->t2, &new_ptr->t2);
 }
 
 struct MyStateOption {
@@ -164,15 +192,16 @@ struct MyStateOption {
     struct MyState_ value;
 };
 
-void promote_my_state_option(struct MyStateOption *old_ptr,
-                             struct MyStateOption const *new_ptr) {
-    if (old_ptr == new_ptr) {
+static void promote_my_state_option(struct MyStateOption *old_ptr,
+                                    struct MyStateOption const *new_ptr) {
+    if (same_ptr(old_ptr, new_ptr)) {
         return;
     }
 
     // free
     if (new_ptr == NULL) {
         if (old_ptr->some) {
+            old_ptr->some = false;
             promote_my_state(&old_ptr->value, NULL);
         }
         return;
@@ -317,19 +346,80 @@ static void activate(GtkApplication *app, gpointer _data) {
     gtk_widget_show_all(window);
 }
 
+static void dump(struct MyStateOption const *s) {
+    if (!s->some) {
+        fprintf(stderr, "state = None.\n");
+        return;
+    }
+
+    fprintf(stderr, "state = Some\n");
+    fprintf(stderr, "  {.t0 = %d,\n", s->value.t0);
+
+    for (int i = 0; i < 2; i++) {
+        struct IntOptionList const *xs = i == 0 ? s->value.t1 : s->value.t2;
+        if (xs == NULL) {
+            fprintf(stderr, "   .t%d = []\n", i + 1);
+        } else {
+            fprintf(stderr, "   .t%d =        %p\n", i + 1, xs);
+            char const *prefix = "[";
+
+            for (; xs != NULL; xs = xs->tail) {
+                char const *sep = xs->tail == NULL ? " ]" : "";
+                if (xs->head.some) {
+                    fprintf(stderr, "    %s Some %d%s      -> %p\n", prefix,
+                            xs->head.value, sep, xs->tail);
+                } else {
+                    fprintf(stderr, "    %s None%s      -> %p\n", prefix, sep,
+                            xs->tail);
+                }
+
+                prefix = " ";
+            }
+        }
+    }
+    fprintf(stderr, "  }\n");
+}
+
 int app_main(void const *init, UpdateFn update, ViewFn view) {
-    s_state = init;
-    s_update = update;
-    s_view = view;
+    // s_state = init;
+    // s_update = update;
+    // s_view = view;
 
-    GtkApplication *app =
-        gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    s_app = app;
+    // GtkApplication *app =
+    //     gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    // s_app = app;
 
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    // g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-    int status = g_application_run(G_APPLICATION(app), 0, NULL);
-    s_app = NULL;
-    g_object_unref(app);
-    return status;
+    // int status = g_application_run(G_APPLICATION(app), 0, NULL);
+    // s_app = NULL;
+    // g_object_unref(app);
+    // return status;
+
+    static struct MyStateOption state = {.some = false};
+    {
+        struct MyStateOption s = {true, {.t0 = 42, .t1 = NULL}};
+        promote_my_state_option(&state, &s);
+    }
+    dump(&state);
+    {
+        struct MyStateOption s = state;
+        struct IntOptionList cons = {.head = {.some = true, .value = 42},
+                                     .tail = NULL};
+        s.value.t1 = &cons;
+        promote_my_state_option(&state, &s);
+    }
+    dump(&state);
+    {
+        struct MyStateOption s = state;
+        struct IntOptionList cons = {.head = {.some = true, .value = 42},
+                                     .tail = s.value.t1};
+        s.value.t1 = &cons;
+        promote_my_state_option(&state, &s);
+    }
+    dump(&state);
+
+    promote_my_state_option(&state, NULL);
+    dump(&state);
+    return 0;
 }
