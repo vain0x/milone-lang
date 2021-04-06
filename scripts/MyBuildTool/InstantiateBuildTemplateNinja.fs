c7614db64c38dc7c94cb4a9d823ba0fdaee827e1
vain0x <vainzerox@gmail.com>
2021-04-06 21:03:09 +0900

    wip!  [skip ci]

--- scripts/MyBuildTool/InstantiateBuildTemplateNinja.fs
+++ scripts/MyBuildTool/InstantiateBuildTemplateNinja.fs
@@ -3,6 +3,7 @@
 module rec MyBuildTool.InstantiateBuildTemplateNinja
 
 open System.IO
+open System.Text
 
 module FS = MyBuildTool.FileSearch
 
@@ -10,77 +11,21 @@ module FS = MyBuildTool.FileSearch
 // tests
 // -----------------------------------------------
 
-let runTestTemplate = """
-# ------------------------------------------------
-# {{ PATH }} (run)
-# ------------------------------------------------
+// let errorTestTemplate = """
+// # ------------------------------------------------
+// # {{ PATH }} (error)
+// # ------------------------------------------------
 
-rule {{ MILONE_RULE }}
-  description = milone {{ PATH }}
-  command = target/milone compile {{ PATH }} --target-dir {{ TARGET_DIR }} >$out
+// rule {{ MILONE_RULE }}
+//   description = milone {{ PATH }}
+//   command = ! target/milone compile {{ PATH }} --target-dir {{ TARGET_DIR }} >$out
 
-build {{ FILES_TXT }}: $
-  {{ MILONE_RULE }} $
-    | {{ SRC }} $
-      {{ EXPECTED_OUT }} $
-      target/milone
-
-rule {{ MILONE_RULE }}_run
-  description = run_tests run {{ PROJECT }}
-  command = { {{ EXE }}; echo '$$? = '$$?; } >$out
-
-build {{ GENERATED_OUT }}: $
-  {{ MILONE_RULE }}_run $
-    | {{ EXPECTED_OUT }} $
-      {{ EXE }}
-"""
-
-let errorTestTemplate = """
-# ------------------------------------------------
-# {{ PATH }} (error)
-# ------------------------------------------------
-
-rule {{ MILONE_RULE }}
-  description = milone {{ PATH }}
-  command = ! target/milone compile {{ PATH }} --target-dir {{ TARGET_DIR }} >$out
-
-build {{ GENERATED_OUT }}: $
-  {{ MILONE_RULE }} $
-    | {{ SRC }} $
-      {{ EXPECTED_OUT }} $
-      target/milone
-"""
-
-let testCollectionTemplate = """
-# ------------------------------------------------
-# Tests
-# ------------------------------------------------
-
-rule build_run_tests
-  description = build_run_tests
-  command = $my_build_tool --build-run-tests
-
-build {{ EXE_FILES }}: $
-  build_run_tests | {{ RUN_TEST_INPUTS }}
-
-build run_tests: $
-  phony {{ EXE_FILES }}
-
-build error_tests: $
-  phony {{ ERROR_TEST_INPUTS }}
-
-
-
-rule summarize_tests
-  description = summarize_tests
-  command = $my_build_tool --summarize-tests
-
-build target/summarize_tests.timestamp: $
-  summarize_tests $
-    | run_tests error_tests
-
-build tests: phony target/summarize_tests.timestamp
-"""
+// build {{ GENERATED_OUT }}: $
+//   {{ MILONE_RULE }} $
+//     | {{ SRC }} $
+//       {{ EXPECTED_OUT }} $
+//       target/milone
+// """
 
 // -----------------------------------------------
 
@@ -98,7 +43,7 @@ type TestProject =
 
 let testProject (categoryDir: string, projectDir, projectName, sources) =
   let categoryName = Path.GetFileName(categoryDir)
-  let targetDir = $"target/{categoryName}/{projectName}"
+  let targetDir = $"target/tests/{categoryName}/{projectName}"
 
   { CategoryDir = categoryDir
     ProjectDir = projectDir
@@ -116,17 +61,28 @@ let render () =
   let fsProjects = FS.allFsProjects solutionDir
   let testProjects = FS.allTestProjects solutionDir
 
-  let ninjaTemplate = File.ReadAllText("build-template.ninja")
+  // let ninjaTemplate = File.ReadAllText("build-template.ninja")
 
-  let ninjaTemplate =
-    ninjaTemplate
-      .Replace("{{ FS_PROJ_FILES }}", List.map fst fsProjects |> String.concat " ")
-      .Replace(
-        "{{ MILONE_CLI_SRC }}",
-        fsProjects
-        |> List.collect snd
-        |> String.concat " "
-      )
+  let myBuildToolInput =
+    seq {
+      for proj, src in fsProjects do
+        if proj.StartsWith("scripts/MyBuildTool") then
+          yield proj
+          yield! src
+    }
+    |> String.concat " "
+
+  let miloneCliInput =
+    seq {
+      for proj, src in fsProjects do
+        if
+          proj.StartsWith("src")
+          || proj.StartsWith("milone_libs")
+        then
+          yield proj
+          yield! src
+    }
+    |> String.concat " "
 
   let runTests, errorTests =
     let readExpectedOutput t =
@@ -158,63 +114,134 @@ let render () =
     |> List.partition fst
     |> (fun (x, y) -> List.map (snd >> testProject) x, List.map (snd >> testProject) y)
 
-  let test =
+  let ninja = StringBuilder()
+  let w (s: string) = ninja.AppendLine(s) |> ignore
+
+  w """
+builddir = target/tests
+timestamp = target/.timestamp
+
+my_build_tool = scripts/MyBuildTool/bin/Debug/net5.0/MyBuildTool
+milone_dotnet = src/MiloneCli/bin/Debug/net5.0/MiloneCli
+
+rule build_my_build_tool
+  description = build_my_build_tool
+  command = dotnet build -nologo scripts/MyBuildTool
+
+rule build_milone_dotnet
+  description = build_milone_dotnet
+  command = dotnet build -nologo src/MiloneCli
+"""
+
+  w $"build $my_build_tool: build_my_build_tool {myBuildToolInput}"
+  w $"build $milone_dotnet: build_milone_dotnet {miloneCliInput}"
+
+  for t in runTests do
+    let id = $"tests_{t.CategoryName}_{t.ProjectName}"
+    let src = String.concat " " t.Sources
+
+    w $"""
+# ------------------------------------------------
+# {t.ProjectDir} (run)
+# ------------------------------------------------
+
+rule {id}_build
+  description = milone {t.ProjectDir}
+  command = target/milone compile {t.ProjectDir} --target-dir {t.TargetDir} >$out
+
+build {t.FilesTxt}: $
+  {id}_build $
+    | {src} $
+      {t.ExpectedOut} $
+      target/milone
+
+rule {id}_run
+  description = run {t.ProjectDir}
+  command = {{ {t.Exe}; echo '$$? = '$$?; }} >$out
+
+build {t.GeneratedOut}: $
+  {id}_run $
+    | {t.ExpectedOut} $
+      {t.Exe}
+"""
+
+  // for t in errorTests do
+    //         |> List.map
+    //              (fun (t: TestProject) ->
+    //                errorTestTemplate
+    //                  .Replace("{{ PATH }}", t.ProjectDir)
+    //                  .Replace("{{ PROJECT }}", t.ProjectName)
+    //                  .Replace("{{ MILONE_RULE }}", t.ProjectName + "_build")
+    //                  .Replace("{{ SRC }}", String.concat " " t.Sources)
+    //                  .Replace("{{ TARGET_DIR }}", t.TargetDir)
+    //                  .Replace("{{ GENERATED_OUT }}", t.GeneratedOut)
+    //                  .Replace("{{ EXPECTED_OUT }}", t.ExpectedOut)))
+    // |> String.concat "\n"
+    // ()
+
+  let exeFiles =
     runTests
-    |> List.map
-         (fun (t: TestProject) ->
-           runTestTemplate
-             .Replace("{{ PATH }}", t.ProjectDir)
-             .Replace("{{ PROJECT }}", t.ProjectName)
-             .Replace("{{ MILONE_RULE }}", t.ProjectName + "_build")
-             .Replace("{{ SRC }}", String.concat " " t.Sources)
-             .Replace("{{ TARGET_DIR }}", t.TargetDir)
-             .Replace("{{ EXE }}", t.Exe)
-             .Replace("{{ EXPECTED_OUT }}", t.ExpectedOut)
-             .Replace("{{ FILES_TXT }}", t.FilesTxt)
-             .Replace("{{ GENERATED_OUT }}", t.GeneratedOut))
-    |> fun items ->
-         List.append
-           items
-           (errorTests
-            |> List.map
-                 (fun (t: TestProject) ->
-                   errorTestTemplate
-                     .Replace("{{ PATH }}", t.ProjectDir)
-                     .Replace("{{ PROJECT }}", t.ProjectName)
-                     .Replace("{{ MILONE_RULE }}", t.ProjectName + "_build")
-                     .Replace("{{ SRC }}", String.concat " " t.Sources)
-                     .Replace("{{ TARGET_DIR }}", t.TargetDir)
-                     .Replace("{{ GENERATED_OUT }}", t.GeneratedOut)
-                     .Replace("{{ EXPECTED_OUT }}", t.ExpectedOut)))
-    |> String.concat "\n"
+    |> List.map (fun (t: TestProject) -> t.Exe)
+    |> String.concat " "
 
-  let collect =
-    let exeFiles =
-      runTests
-      |> List.map (fun (t: TestProject) -> t.Exe)
-      |> String.concat " "
+  let generatedOuts =
+    runTests
+    |> List.map (fun (t: TestProject) -> t.GeneratedOut)
+    |> String.concat " "
 
-    let runTestInputs =
-      runTests
-      |> List.collect (fun (t: TestProject) -> t.ExpectedOut :: t.Sources)
-      |> String.concat " "
+  let runTestInputs =
+    runTests
+    |> List.collect (fun (t: TestProject) -> t.ExpectedOut :: t.FilesTxt :: t.Sources)
+    |> String.concat " "
 
-    let errorTestInputs =
-      errorTests
-      |> List.collect (fun (t: TestProject) -> t.ExpectedOut :: t.Sources)
-      |> String.concat " "
+  let runTestProjectDirs =
+    runTests
+    |> Seq.map (fun (t: TestProject) -> t.ProjectDir)
+    |> String.concat " "
 
-    testCollectionTemplate
-      .Replace("{{ EXE_FILES }}", exeFiles)
-      .Replace("{{ RUN_TEST_INPUTS }}", runTestInputs)
-      .Replace("{{ ERROR_TEST_INPUTS }}", errorTestInputs)
+  // let errorTestInputs =
+  //   errorTests
+  //   |> List.collect (fun (t: TestProject) -> t.ExpectedOut :: t.Sources)
+  //   |> String.concat " "
 
-  ninjaTemplate
-    .Replace("{{ FSPROJ_FILES }}", fsProjects |> List.map fst |> String.concat " ")
-    .Replace(
-      "{{ MILONE_CLI_SRC }}",
-      fsProjects
-      |> List.collect (fun (x, xs) -> x :: xs)
-      |> String.concat " "
-    )
-    .Replace("{{ TEST }}", test + "\n" + collect)
+  w $"""
+# ------------------------------------------------
+# Tests
+# ------------------------------------------------
+
+rule build_run_tests
+  description = build_run_tests
+  command = $my_build_tool --build-run-tests {runTestProjectDirs}
+
+build {exeFiles}: $
+  build_run_tests $
+    | $my_build_tool runtime/milone.o runtime/milone.h $
+      {runTestInputs}
+
+build run_tests: $
+  phony {exeFiles}
+
+
+
+rule summarize_tests
+  description = summarize_tests
+  command = $my_build_tool --summarize-tests {runTestProjectDirs} && touch $out
+
+build target/.timestamp/summarize_tests: $
+  summarize_tests $
+    | run_tests $my_build_tool {generatedOuts}
+
+build tests: phony target/.timestamp/summarize_tests
+"""
+
+  // ninjaTemplate
+  //   .Replace("{{ FSPROJ_FILES }}", fsProjects |> List.map fst |> String.concat " ")
+  //   .Replace(
+  //     "{{ MILONE_CLI_SRC }}",
+  //     fsProjects
+  //     |> List.collect (fun (x, xs) -> x :: xs)
+  //     |> String.concat " "
+  //   )
+  //   .Replace("{{ TEST }}", test + "\n" + collect)
+
+  ninja.ToString()
--- scripts/MyBuildTool/Program.fs
+++ scripts/MyBuildTool/Program.fs
@@ -8,16 +8,59 @@ open System.Text
 module FS = MyBuildTool.FileSearch
 
 let private helpText () =
-  """MyBuildTool: Build tool for milone-lang project."""
+  """MyBuildTool: Build tool for milone-lang project.
 
-let binNinja = "bin/ninja"
+SUBCOMMANDS:
+    gen2    generates target/milone
+    gen3    compiles the compiler with target/milone
+            and verify the result.
 
-let entities = "fsproj"
+    tests   tests projects in the `tests` directory
 
-let private gen1Bin =
-  "src/MiloneCli/bin/Debug/net5.0/MiloneCli"
+    help, -h, --help
+    version, -V, --version
+"""
 
-let private gen2Bin = "target/milone"
+let private cRules = """
+warning_flags = $
+  -Wall $
+  -Wextra $
+  -Wpedantic $
+  -Wno-parentheses-equality $
+  -Wno-pointer-to-int-cast $
+  -Wno-unknown-warning-option $
+  -Wno-unused-but-set-parameter $
+  -Wno-unused-but-set-variable $
+  -Wno-unused-const-variable $
+  -Wno-unused-function $
+  -Wno-unused-label $
+  -Wno-unused-parameter $
+  -Wno-unused-variable
+
+rule compile_c_to_obj
+  description = compile_c_to_obj $in
+  command = $${CC:-gcc} -std=c11 $warning_flags -O1 -g -c -Iruntime $in -o $out
+
+rule link_objs_to_exe
+  description = link_objs_to_exe $out
+  command = $${CC:-gcc} $in -o $out
+
+build runtime/milone.o: $
+  compile_c_to_obj $
+    runtime/milone.c $
+    | runtime/milone.h
+"""
+
+let private makeDir (dir: string) =
+  Directory.CreateDirectory(dir) |> ignore
+
+let private readToDiff (file: string) =
+  try
+    if File.Exists(file) then
+      File.ReadAllText(file)
+    else
+      $"{file} doesn't exist"
+  with _ -> $"{file} couldn't be read."
 
 let private writeTo (contents: string) (output: string) =
   let same =
@@ -29,186 +72,293 @@ let private writeTo (contents: string) (output: string) =
   if not same then
     File.WriteAllText(output, contents)
 
+let private run command args =
+  let startInfo = ProcessStartInfo(command)
+
+  for arg in args do
+    startInfo.ArgumentList.Add(arg)
+
+  let p = Process.Start(startInfo)
+  p.WaitForExit()
+  if p.ExitCode <> 0 then exit 1
+
+let private runToOut command args =
+  let startInfo = ProcessStartInfo(command)
+
+  for arg in args do
+    startInfo.ArgumentList.Add(arg)
+
+  startInfo.RedirectStandardOutput <- true
+  startInfo.UseShellExecute <- false
+
+  let p = Process.Start(startInfo)
+  p.WaitForExit()
+  if p.ExitCode <> 0 then exit 1
+
+  p.StandardOutput.ReadToEnd()
+
+let private commandGen2 () =
+  eprintfn "milone-compiling gen2"
+  makeDir "target/gen2"
+
+  let stdOut =
+    runToOut
+      "dotnet"
+      [ "run"
+        "-p"
+        "src/MiloneCli"
+        "--"
+        "compile"
+        "src/MiloneCli"
+        "--target-dir"
+        "target/gen2" ]
+
+  let ninja = StringBuilder()
+
+  ninja.AppendLine("builddir = target/gen2")
+  |> ignore
+
+  ninja.AppendLine(cRules) |> ignore
+
+  let oFiles = ResizeArray()
+
+  for name in stdOut.Split("\n") do
+    let name =
+      Path.GetFileNameWithoutExtension(name.Trim())
+
+    if String.IsNullOrEmpty(name) |> not then
+      let c = $"target/gen2/{name}.c"
+      let o = $"target/gen2/{name}.o"
+      oFiles.Add(o)
+
+      ninja.AppendLine($"build {o}: compile_c_to_obj {c} | runtime/milone.h")
+      |> ignore
+
+  let input = oFiles |> String.concat " "
+
+  ninja.AppendLine($"build target/milone: link_objs_to_exe runtime/milone.o {input}")
+  |> ignore
+
+  writeTo (ninja.ToString()) "target/gen2/build.ninja"
+
+  eprintfn "c-compiling gen2"
+
+  run
+    "bin/ninja"
+    [ "-f"
+      "target/gen2/build.ninja"
+      "target/milone" ]
+
+let private commandGen3 () =
+  eprintfn "milone-compiling gen3"
+  makeDir "target/gen3"
+
+  let stdOut =
+    runToOut
+      "target/milone"
+      [ "compile"
+        "--profile"
+        "src/MiloneCli"
+        "--target-dir"
+        "target/gen3" ]
+
+  // compare
+  let mutable ok = true
+
+  for name in stdOut.Split("\n") do
+    let name =
+      Path.GetFileNameWithoutExtension(name.Trim())
+
+    if name <> "" then
+      let c2 = $"target/gen2/{name}.c"
+      let c3 = $"target/gen3/{name}.c"
+
+      if readToDiff c2 <> readToDiff c3 then
+        printfn $"differ: {c2} vs {c3}"
+        ok <- true
+
+  if not ok then exit 1
+
+let private commandTests () =
+  writeTo (InstantiateBuildTemplateNinja.render ()) "target/tests.ninja"
+
+  run "ninja" [ "-f"; "target/tests.ninja"; "tests" ]
+
+let private commandTestsBuild (testProjectDirs: string list) =
+  let ninja = StringBuilder()
+  let w (s: string) = ninja.AppendLine(s) |> ignore
+
+  w "builddir = target/tests2"
+  w cRules
+
+  let exeFiles = ResizeArray()
+
+  for projectDir in testProjectDirs do
+    let projectName = Path.GetFileName(projectDir)
+
+    let category =
+      Path.GetFileName(Path.GetDirectoryName(projectDir))
+
+    let targetDir = $"target/tests/{category}/{projectName}"
+    let exe = $"{targetDir}/{projectName}.exe"
+    exeFiles.Add(exe)
+
+    w "\n# ----------------------------------"
+    w $"# {projectDir}"
+    w "# ----------------------------------"
+
+    let files =
+      File.ReadAllText($"{targetDir}/files.txt")
+
+    let oFiles = ResizeArray()
+
+    for name in files.Split("\n") do
+      let name =
+        Path.GetFileNameWithoutExtension(name.Trim())
+
+      if String.IsNullOrEmpty(name) |> not then
+        let c = $"{targetDir}/{name}.c"
+        let o = $"{targetDir}/{name}.o"
+        oFiles.Add(o)
+
+        w $"build {o}: compile_c_to_obj {c} | runtime/milone.h"
+
+    let input = oFiles |> String.concat " "
+
+    w $"build {exe}: link_objs_to_exe runtime/milone.o {input}"
+
+  let exeFiles = String.concat " " exeFiles
+
+  w $"default {exeFiles}"
+
+  writeTo (ninja.ToString()) "target/tests2-build.ninja"
+
+  eprintfn "c-compiling test projects"
+
+  run "bin/ninja" [ "-f"; "target/tests2-build.ninja" ]
+
+let private commandTestsSummarize (testProjectDirs: string list) =
+  let mutable pass = 0
+  let mutable fail = 0
+
+  for projectDir in testProjectDirs do
+    let projectName = Path.GetFileName(projectDir)
+
+    let category =
+      Path.GetFileName(Path.GetDirectoryName(projectDir))
+
+    let targetDir = $"target/tests/{category}/{projectName}"
+    let expectedOut = $"{projectDir}/{projectName}.out"
+    let generatedTxt = $"{targetDir}/generated.txt"
+
+    if readToDiff expectedOut <> readToDiff generatedTxt then
+      printfn $"differ {generatedTxt} vs {expectedOut}"
+      fail <- fail + 1
+    else
+      pass <- pass + 1
+
+  let ok = pass <> 0 && fail = 0
+  printfn "pass %d / fail %d / total %d" pass fail (pass + fail)
+  printfn "%s" (if ok then "OK" else "FAILED")
+  if not ok then exit 1
+
 [<EntryPoint>]
 let main argv =
   match Array.toList argv with
   | []
   | "help" :: _
   | "-h" :: _
-  | "--help" :: _ ->
-      printfn "%s" (helpText ())
-      0
+  | "--help" :: _ -> printfn "%s" (helpText ())
 
   | "version" :: _
   | "-V" :: _
-  | "--version" :: _ ->
-      printfn "0.1.0"
-      0
+  | "--version" :: _ -> printfn "0.1.0"
 
-  | "gen2" :: _ ->
+  | "gen2" :: _ -> commandGen2 ()
+  | "gen3" :: _ -> commandGen3 ()
 
-      let startInfo = ProcessStartInfo("dotnet")
-      startInfo.ArgumentList.Add("run")
-      startInfo.ArgumentList.Add("-p")
-      startInfo.ArgumentList.Add("src/MiloneCli")
-      startInfo.ArgumentList.Add("--")
-      startInfo.ArgumentList.Add("compile")
-      startInfo.ArgumentList.Add("src/MiloneCli")
-      startInfo.ArgumentList.Add("--target-dir")
-      startInfo.ArgumentList.Add("target/gen2")
-      startInfo.RedirectStandardOutput <- true
-      startInfo.UseShellExecute <- false
-      let p = Process.Start(startInfo)
-      p.WaitForExit()
-      let stdOut = p.StandardOutput.ReadToEnd()
+  | "tests" :: _ -> commandTests ()
+  | "--build-run-tests" :: args -> commandTestsBuild args
+  | "--summarize-tests" :: args -> commandTestsSummarize args
 
-      if p.ExitCode <> 0 then
-        1
-      // eprintfn "out = %A, %d" (stdOut) p.ExitCode
-      else
+  // | "milone" :: projectDir :: envs ->
+  //     let _, map =
+  //       envs
+  //       |> List.fold
+  //            (fun (parity, map) (e: string) ->
+  //              if parity = 0 then
+  //                if e = "-e" then
+  //                  1, map
+  //                else
+  //                  failwithf "Expected -e, %A" e
+  //              else
+  //                match e.Split("=")
+  //                      |> Array.map (fun (s: string) -> s.Trim()) with
+  //                | [| name; value |] -> 0, Map.add name value map
+  //                | _ -> failwithf "invalid env %A" e)
+  //            (0, Map.empty)
 
-        let ninja = StringBuilder()
+  //     let env (name: string) =
+  //       match Map.tryFind name map with
+  //       | Some it -> it
+  //       | None ->
+  //           match Environment.GetEnvironmentVariable(name) with
+  //           | null
+  //           | "" ->
+  //               match name with
+  //               | "MILONE_HOME" ->
+  //                   Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+  //                   + "/.milone"
+  //               | "MILONE" -> "milone"
+  //               | "EXE" -> "a.out"
+  //               | "VERBOSITY" -> "--quiet"
+  //               | "TARGET_DIR" -> "target"
+  //               | _ -> failwithf "env missing %A" name
+  //           | it -> it
 
-        """
-builddir = target/gen2
+  //     eprintfn "projectDir=%s map=%A" projectDir map
 
-rule compile_c_to_obj
-  description = compile_c_to_obj $in
-  command = $${CC:-gcc} -std=c11 $warning_flags -O1 -g -c -Iruntime $in -o $out
+  // $my_build_tool milone src/MiloneCli $
+  //   -e MILONE_HOME=$$PWD $
+  //   -e MILONE=$milone_dotnet_debug $
+  //   -e EXE=$out $
+  //   -e TARGET_DIR=target/gen2 $
+  //   -e VERBOSITY=$verbosity $
+  //   >$target_dir/build.ninja && $
 
-rule link_objs_to_exe
-  description = link_objs_to_exe
-  command = $${CC:-gcc} $in -o $out
+  // | "milone-delete-c-files" :: targetDir -> ()
 
-build runtime/milone.o: $
-  compile_c_to_obj $
-    runtime/milone.c $
-    | runtime/milone.h
-"""
-        |> ninja.AppendLine
-        |> ignore
+  // Stage 1: Install 'bin/ninja'.
+  // FIXME: impl
 
-        let oFiles = ResizeArray()
+  // Stage 2: Generate the default 'build.ninja'.
+  // let workspaceDir = Environment.CurrentDirectory
+  // let fsProjects = FS.allFsProjects workspaceDir
+  // let testProjects = FS.allTestProjects workspaceDir
 
-        for name in stdOut.Split("\n") do
-          let name =
-            Path.GetFileNameWithoutExtension(name.Trim())
+  // let stage2 () =
+  //   let input =
+  //     [ yield "build-template.ninja"
 
-          if String.IsNullOrEmpty(name) |> not then
-            let c = $"target/gen2/{name}.c"
-            let o = $"target/gen2/{name}.o"
-            oFiles.Add(o)
+  //       for projectFile, sourceFiles in fsProjects do
+  //         yield projectFile
+  //         yield! sourceFiles
 
-            ninja.AppendLine($"build {o}: compile_c_to_obj {c} | runtime/milone.h")
-            |> ignore
+  //       for _, projectDir, _, _ in testProjects do
+  //         yield projectDir ]
 
-        let input = oFiles |> String.concat " "
+  //   let output = "target/build-meta.ninja"
 
-        ninja.AppendLine($"build target/milone: link_objs_to_exe runtime/milone.o {input}")
-        |> ignore
+  //   let ninjaTemplate = File.ReadAllText("build-template.ninja")
 
-        writeTo (ninja.ToString()) "target/gen2/build.ninja"
+  //   output |> writeTo (InstantiateBuildTemplateNinja.render ())
 
-        let startInfo = ProcessStartInfo("bin/ninja")
-        startInfo.ArgumentList.Add("-f")
-        startInfo.ArgumentList.Add("target/gen2/build.ninja")
-        startInfo.ArgumentList.Add("target/milone")
-        startInfo.UseShellExecute <- false
-        let p = Process.Start(startInfo)
-        p.WaitForExit()
+  // Stage 3: Run ninja-1.
 
-        if p.ExitCode <> 0 then
-          p.ExitCode
-        else
-          0
+  // Stage 4: Re-run ninja-2.
 
-  | "milone" :: projectDir :: envs ->
-      let _, map =
-        envs
-        |> List.fold
-             (fun (parity, map) (e: string) ->
-               if parity = 0 then
-                 if e = "-e" then
-                   1, map
-                 else
-                   failwithf "Expected -e, %A" e
-               else
-                 match e.Split("=")
-                       |> Array.map (fun (s: string) -> s.Trim()) with
-                 | [| name; value |] -> 0, Map.add name value map
-                 | _ -> failwithf "invalid env %A" e)
-             (0, Map.empty)
+  | _ ->
+      eprintfn "unknown target: %A" argv
+      exit 1
 
-      let env (name: string) =
-        match Map.tryFind name map with
-        | Some it -> it
-        | None ->
-            match Environment.GetEnvironmentVariable(name) with
-            | null
-            | "" ->
-                match name with
-                | "MILONE_HOME" ->
-                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
-                    + "/.milone"
-                | "MILONE" -> "milone"
-                | "EXE" -> "a.out"
-                | "VERBOSITY" -> "--quiet"
-                | "TARGET_DIR" -> "target"
-                | _ -> failwithf "env missing %A" name
-            | it -> it
-
-      eprintfn "projectDir=%s map=%A" projectDir map
-
-
-
-      // $my_build_tool milone src/MiloneCli $
-      //   -e MILONE_HOME=$$PWD $
-      //   -e MILONE=$milone_dotnet_debug $
-      //   -e EXE=$out $
-      //   -e TARGET_DIR=target/gen2 $
-      //   -e VERBOSITY=$verbosity $
-      //   >$target_dir/build.ninja && $
-      0
-
-  | "milone-delete-c-files" :: targetDir -> 0
-
-  | "--build-run-tests" :: _ ->
-      printfn "summarize-tests"
-      0
-
-  | "--summarize-tests" :: _ ->
-      printfn "summarize-tests"
-      0
-
-  | targets ->
-      // Stage 1: Install 'bin/ninja'.
-      // FIXME: impl
-
-      // Stage 2: Generate the default 'build.ninja'.
-      // let workspaceDir = Environment.CurrentDirectory
-      // let fsProjects = FS.allFsProjects workspaceDir
-      // let testProjects = FS.allTestProjects workspaceDir
-
-      // let stage2 () =
-      //   let input =
-      //     [ yield "build-template.ninja"
-
-      //       for projectFile, sourceFiles in fsProjects do
-      //         yield projectFile
-      //         yield! sourceFiles
-
-      //       for _, projectDir, _, _ in testProjects do
-      //         yield projectDir ]
-
-      //   let output = "target/build-meta.ninja"
-
-      //   let ninjaTemplate = File.ReadAllText("build-template.ninja")
-
-      //   output |> writeTo (InstantiateBuildTemplateNinja.render ())
-
-      // Stage 3: Run ninja-1.
-
-      // Stage 4: Re-run ninja-2.
-
-      printfn "%s" (InstantiateBuildTemplateNinja.render ())
-      0
+  0
