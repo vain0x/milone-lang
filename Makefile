05d3f5eabbb431efbc779315bbba83b3f1464dd1
vain0x <vainzerox@gmail.com>
2021-04-07 23:11:13 +0900

    chore: Implement MyBuildTool

--- Makefile
+++ Makefile
@@ -1,59 +1,20 @@
-# Makefile as a thin wrapper of ninja.
+# Makefile as a thin wrapper of MyBuildTool.
 #
 # USAGE:
 #    make
 #    make install
 #    make install-dev
 
-default: target/gen3_files.txt
+default: test
 
 .PHONY: build clean default install install-dev test uninstall
 
 # ------------------------------------------------
-# temporary build rules
+# entrypoints
 # ------------------------------------------------
-
-src/MiloneCli/bin/Debug/net5.0/MiloneCli: \
-	$(wildcard src/*/*.fs) \
-	$(wildcard src/*/*.fsproj) \
-	$(wildcard src/*/*.milone)
-	dotnet build --no-restore src/MiloneCli && \
-	touch src/MiloneCli/bin/Debug/net5.0/MiloneCli
-
-target/gen2_files.txt: src/MiloneCli/bin/Debug/net5.0/MiloneCli
-	src/MiloneCli/bin/Debug/net5.0/MiloneCli compile src/MiloneCli --profile --target-dir target/gen2 | sed 's;^;target/gen2/;' | tee target/gen2_files.txt >&2
-
-target/milone: target/gen2_files.txt
-	cat target/gen2_files.txt | \
-		xargs -I{} sh -c 'clang -Wno-parentheses-equality -Iruntime -c {} -o $$(dirname {})/$$(basename {} .c).o' && \
-	clang runtime/milone.o $(shell cat target/gen2_files.txt | sed 's;\.c$$;.o;') -o target/milone
-
-target/gen3_files.txt: target/milone
-	target/milone compile src/MiloneCli --profile --target-dir target/gen3 | sed 's;^;target/gen3/;' | tee target/gen3_files.txt >&2 && \
-		if scripts/verify-gen3; then echo 'OK'; else echo 'NG'; fi
-
-# ------------------------------------------------
-# ninja wrapper
-# ------------------------------------------------
-
-build.ninja: \
-		$(wildcard scripts/GenNinjaFile/*.fs) \
-		$(wildcard scripts/GenNinjaFile/*.fsproj) \
-		build-template.ninja \
-		$(wildcard tests/*)
-	scripts/build-ninja-gen
-
-build: bin/ninja build.ninja
-	bin/ninja test_self
-
-test: build
 
 clean:
 	scripts/clean
-
-# ------------------------------------------------
-# install
-# ------------------------------------------------
 
 install:
 	scripts/install
@@ -70,3 +31,47 @@ uninstall:
 
 bin/ninja:
 	scripts/install-ninja
+
+# ------------------------------------------------
+# MyBuildTool wrapper
+# ------------------------------------------------
+
+MY_BUILD := scripts/MyBuildTool/bin/Debug/net5.0/MyBuildTool
+
+.PHONY: dotnet_restore gen2 gen3 integration_tests my_build self test_self
+
+target/.timestamp/dotnet_restore: \
+		$(wildcard **/*.fsproj)
+	dotnet restore && touch $@
+
+${MY_BUILD}: target/.timestamp/dotnet_restore \
+		$(wildcard scripts/MyBuildTool/*.fs) \
+		$(wildcard scripts/MyBuildTool/*.fsproj)
+	dotnet build -nologo scripts/MyBuildTool
+
+my_build: ${MY_BUILD}
+
+target/milone: bin/ninja ${MY_BUILD} \
+		$(wildcard src/*/*.fs) \
+		$(wildcard src/*/*.fsproj) \
+		$(wildcard src/*/*.milone)
+	${MY_BUILD} gen2
+
+gen2: target/milone
+
+target/.timestamp/gen3: bin/ninja ${MY_BUILD} target/milone
+	${MY_BUILD} gen3 && touch $@
+
+gen3: target/.timestamp/gen3
+
+self: gen2
+
+test_self: gen3
+
+integration_tests: bin/ninja ${MY_BUILD} target/milone \
+		$(wildcard tests/**/*.fs) \
+		$(wildcard tests/**/*.milone) \
+		$(wildcard tests/**/*.out)
+	${MY_BUILD} tests
+
+test: test_self integration_tests
--- scripts/MyBuildTool/Program.fs
+++ scripts/MyBuildTool/Program.fs
@@ -1,9 +1,267 @@
 module rec MyBuildTool.Program
 
 open System
+open System.Diagnostics
+open System.IO
+open System.Text
+
+module FS = MyBuildTool.FileSearch
 
 let private helpText () =
-  """MyBuildTool: Build tool for milone-lang project."""
+  """MyBuildTool: Build tool for milone-lang project.
+
+SUBCOMMANDS:
+    gen2    generates target/milone
+    gen3    compiles the compiler with target/milone
+            and verify the result.
+
+    tests   tests projects in the `tests` directory
+
+    help, -h, --help
+    version, -V, --version
+"""
+
+let private cRules = """
+warning_flags = $
+  -Wall $
+  -Wextra $
+  -Wpedantic $
+  -Wno-parentheses-equality $
+  -Wno-pointer-to-int-cast $
+  -Wno-unknown-warning-option $
+  -Wno-unused-but-set-parameter $
+  -Wno-unused-but-set-variable $
+  -Wno-unused-const-variable $
+  -Wno-unused-function $
+  -Wno-unused-label $
+  -Wno-unused-parameter $
+  -Wno-unused-variable
+
+rule compile_c_to_obj
+  description = compile_c_to_obj $in
+  command = $${CC:-gcc} -std=c11 $warning_flags -O1 -g -c -Iruntime $in -o $out
+
+rule link_objs_to_exe
+  description = link_objs_to_exe $out
+  command = $${CC:-gcc} $in -o $out
+
+build runtime/milone.o: $
+  compile_c_to_obj $
+    runtime/milone.c $
+    | runtime/milone.h
+"""
+
+let private makeDir (dir: string) =
+  Directory.CreateDirectory(dir) |> ignore
+
+let private readToDiff (file: string) =
+  try
+    if File.Exists(file) then
+      File.ReadAllText(file)
+    else
+      $"{file} doesn't exist"
+  with _ -> $"{file} couldn't be read."
+
+let private writeTo (contents: string) (output: string) =
+  let same =
+    try
+      File.Exists(output)
+      && File.ReadAllText(output) = contents
+    with _ -> false
+
+  if not same then
+    File.WriteAllText(output, contents)
+
+let private run command args =
+  let startInfo = ProcessStartInfo(command)
+
+  for arg in args do
+    startInfo.ArgumentList.Add(arg)
+
+  let p = Process.Start(startInfo)
+  p.WaitForExit()
+
+  if p.ExitCode <> 0 then
+    eprintfn "ERROR: '%s' %A exited with %d." command args p.ExitCode
+    exit 1
+
+let private runToOut command args =
+  let startInfo = ProcessStartInfo(command)
+
+  for arg in args do
+    startInfo.ArgumentList.Add(arg)
+
+  startInfo.RedirectStandardOutput <- true
+  startInfo.UseShellExecute <- false
+
+  let p = Process.Start(startInfo)
+  p.WaitForExit()
+
+  let output = p.StandardOutput.ReadToEnd()
+
+  if p.ExitCode <> 0 then
+    printfn "%s\nERROR: '%s' %A exited with %d." output command args p.ExitCode
+    exit 1
+
+  output
+
+let private commandGen2 () =
+  eprintfn "milone-compiling gen2"
+  makeDir "target/gen2"
+
+  let stdOut =
+    runToOut
+      "dotnet"
+      [ "run"
+        "-p"
+        "src/MiloneCli"
+        "--"
+        "compile"
+        "src/MiloneCli"
+        "--target-dir"
+        "target/gen2" ]
+
+  let ninja = StringBuilder()
+  let w (s: string) = ninja.AppendLine(s) |> ignore
+
+  w "builddir = target/gen2"
+  w cRules
+
+  let oFiles = ResizeArray()
+
+  for name in stdOut.Split("\n") do
+    let name =
+      Path.GetFileNameWithoutExtension(name.Trim())
+
+    if String.IsNullOrEmpty(name) |> not then
+      let c = $"target/gen2/{name}.c"
+      let o = $"target/gen2/{name}.o"
+      oFiles.Add(o)
+
+      w $"build {o}: compile_c_to_obj {c} | runtime/milone.h"
+
+  let input = oFiles |> String.concat " "
+
+  w $"build target/milone: link_objs_to_exe runtime/milone.o {input}"
+
+  writeTo (ninja.ToString()) "target/gen2/build.ninja"
+
+  eprintfn "c-compiling gen2"
+
+  run
+    "bin/ninja"
+    [ "-f"
+      "target/gen2/build.ninja"
+      "target/milone" ]
+
+let private commandGen3 () =
+  eprintfn "milone-compiling gen3"
+  makeDir "target/gen3"
+
+  let stdOut =
+    runToOut
+      "target/milone"
+      [ "compile"
+        "--profile"
+        "src/MiloneCli"
+        "--target-dir"
+        "target/gen3" ]
+
+  // compare
+  let mutable ok = true
+
+  for name in stdOut.Split("\n") do
+    let name =
+      Path.GetFileNameWithoutExtension(name.Trim())
+
+    if name <> "" then
+      let c2 = $"target/gen2/{name}.c"
+      let c3 = $"target/gen3/{name}.c"
+
+      if readToDiff c2 <> readToDiff c3 then
+        printfn $"differ: {c2} vs {c3}"
+        ok <- true
+
+  if not ok then exit 1
+
+let private commandTests () =
+  File.WriteAllText("target/tests-build.ninja", InstantiateBuildTemplateNinja.render ())
+
+  run "bin/ninja" [ "-f"; "target/tests-build.ninja" ]
+
+let private commandTestsBuild (testProjectDirs: string list) =
+  let ninja = StringBuilder()
+  let w (s: string) = ninja.AppendLine(s) |> ignore
+
+  w "builddir = target/tests2"
+  w cRules
+
+  let exeFiles = ResizeArray()
+
+  for projectDir in testProjectDirs do
+    let projectName = Path.GetFileName(projectDir)
+
+    let category =
+      Path.GetFileName(Path.GetDirectoryName(projectDir))
+
+    let targetDir = $"target/tests/{category}/{projectName}"
+    let exe = $"{targetDir}/{projectName}.exe"
+    exeFiles.Add(exe)
+
+    w "\n# ----------------------------------"
+    w $"# {projectDir}"
+    w "# ----------------------------------"
+
+    let files =
+      File.ReadAllText($"{targetDir}/files.txt")
+
+    let oFiles = ResizeArray()
+
+    for name in files.Split("\n") do
+      let name =
+        Path.GetFileNameWithoutExtension(name.Trim())
+
+      if String.IsNullOrEmpty(name) |> not then
+        let c = $"{targetDir}/{name}.c"
+        let o = $"{targetDir}/{name}.o"
+        oFiles.Add(o)
+
+        w $"build {o}: compile_c_to_obj {c} | runtime/milone.h"
+
+    let input = oFiles |> String.concat " "
+
+    w $"build {exe}: link_objs_to_exe runtime/milone.o {input}"
+
+  let exeFiles = String.concat " " exeFiles
+
+  w $"default {exeFiles}"
+
+  writeTo (ninja.ToString()) "target/tests2-build.ninja"
+
+let private commandTestsSummarize (testProjectDirs: string list) =
+  let mutable pass = 0
+  let mutable fail = 0
+
+  for projectDir in testProjectDirs do
+    let projectName = Path.GetFileName(projectDir)
+
+    let category =
+      Path.GetFileName(Path.GetDirectoryName(projectDir))
+
+    let targetDir = $"target/tests/{category}/{projectName}"
+    let expectedOut = $"{projectDir}/{projectName}.out"
+    let generatedTxt = $"{targetDir}/generated.txt"
+
+    if readToDiff expectedOut <> readToDiff generatedTxt then
+      printfn $"differ {generatedTxt} vs {expectedOut}"
+      fail <- fail + 1
+    else
+      pass <- pass + 1
+
+  let ok = pass <> 0 && fail = 0
+  printfn "pass %d / fail %d / total %d" pass fail (pass + fail)
+  printfn "%s" (if ok then "OK" else "FAILED")
+  if not ok then exit 1
 
 [<EntryPoint>]
 let main argv =
@@ -11,17 +269,21 @@ let main argv =
   | []
   | "help" :: _
   | "-h" :: _
-  | "--help" :: _ ->
-      printfn "%s" (helpText ())
-      exit 0
+  | "--help" :: _ -> printfn "%s" (helpText ())
 
   | "version" :: _
   | "-V" :: _
-  | "--version" :: _ ->
-      printfn "0.1.0"
-      exit 0
+  | "--version" :: _ -> printfn "0.1.0"
 
-  | arg :: _ ->
-      printfn "ERROR: Unknown arg: '%s'.\n" arg
-      printfn "%s" (helpText ())
+  | "gen2" :: _ -> commandGen2 ()
+  | "gen3" :: _ -> commandGen3 ()
+
+  | "tests" :: _ -> commandTests ()
+  | "--build-run-tests" :: args -> commandTestsBuild args
+  | "--summarize-tests" :: args -> commandTestsSummarize args
+
+  | _ ->
+      eprintfn "unknown target: %A" argv
       exit 1
+
+  0
