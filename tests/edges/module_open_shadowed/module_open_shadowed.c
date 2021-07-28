0d650ae9dd657bd9dd851a65b06ebbb2e9a5af6a
vain0x <vainzerox@gmail.com>
2021-07-29 00:06:34 +0900

    fix: `open` is processed in early pass

--- tests/edges/module_open_shadowed/module_open_shadowed.c
+++ tests/edges/module_open_shadowed/module_open_shadowed.c
@@ -6,8 +6,6 @@ struct String f_(char arg_1);
 
 int module_open_shadowed_sub_f(int);
 
-int module_open_shadowed_sub_g(int);
-
 int milone_main();
 
 struct String module_open_shadowed_Program_g(char arg_) {
@@ -21,9 +19,9 @@ struct String f_(char arg_1) {
 int milone_main() {
     int call_ = module_open_shadowed_sub_f(40);
     milone_assert((call_ == 42), 7, 2);
-    int call_1 = module_open_shadowed_sub_g(0);
-    milone_assert((call_1 == 3), 9, 2);
+    struct String call_1 = module_open_shadowed_Program_g(0);
+    milone_assert((str_compare(call_1, (struct String){.str = "g is shadowed", .len = 13}) == 0), 9, 2);
     struct String call_2 = f_(0);
-    milone_assert((str_compare(call_2, (struct String){.str = "f is locally shadowed", .len = 21}) == 0), 14, 4);
+    milone_assert((str_compare(call_2, (struct String){.str = "f is locally shadowed", .len = 21}) == 0), 13, 4);
     return 0;
 }
--- tests/edges/module_open_shadowed/module_open_shadowed.fs
+++ tests/edges/module_open_shadowed/module_open_shadowed.fs
@@ -5,10 +5,9 @@ open module_open_shadowed.sub
 let g () = "g is shadowed"
 
 let main _ =
-  assert (f 40 = 42)
-  // FIXME: g should be shadowed
-  assert (g 0 = 3)
-  // assert (g () = "g is shadowed")
+  assert (f 40 = 42) // not shadowed
+
+  assert (g () = "g is shadowed")
 
   let () =
     let f () = "f is locally shadowed"
