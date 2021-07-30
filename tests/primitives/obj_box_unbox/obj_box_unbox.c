1360f2af3f954e51454a009f22c2928743f7733c
vain0x <vainzerox@gmail.com>
2021-07-30 21:28:16 +0900

    refactor: Update a test to see if it works

--- tests/primitives/obj_box_unbox/obj_box_unbox.c
+++ tests/primitives/obj_box_unbox/obj_box_unbox.c
@@ -1,8 +1,17 @@
 #include "milone.h"
 
+struct IntList;
+
+struct IntList;
+
 char printIntOrStr_(bool isInt_, void const* value_);
 
 int milone_main();
+
+struct IntList {
+    int head;
+    struct IntList const* tail;
+};
 
 char printIntOrStr_(bool isInt_, void const* value_) {
     char if_;
@@ -28,5 +37,27 @@ int milone_main() {
     void const* box_ = milone_mem_alloc(1, sizeof(struct String));
     (*(((struct String*)box_))) = (struct String){.str = "Fizz", .len = 4};
     char call_1 = printIntOrStr_(false, box_);
+    void const* n_ = ((void const*)((intptr_t)42));
+    milone_assert((((int)((intptr_t)n_)) == 42), 19, 2);
+    struct IntList const* list_1 = milone_mem_alloc(1, sizeof(struct IntList));
+    (*(((struct IntList*)list_1))) = (struct IntList){.head = 2, .tail = NULL};
+    struct IntList const* list_ = milone_mem_alloc(1, sizeof(struct IntList));
+    (*(((struct IntList*)list_))) = (struct IntList){.head = 4, .tail = list_1};
+    void const* xs_ = ((void const*)((intptr_t)list_));
+    char match_;
+    if ((!(((struct IntList const*)((intptr_t)xs_))))) goto next_5;
+    int a_ = ((struct IntList const*)((intptr_t)xs_))->head;
+    if ((!(((struct IntList const*)((intptr_t)xs_))->tail))) goto next_5;
+    int b_ = ((struct IntList const*)((intptr_t)xs_))->tail->head;
+    if ((!((!(((struct IntList const*)((intptr_t)xs_))->tail->tail))))) goto next_5;
+    milone_assert((((a_ * 10) + b_) == 42), 24, 16);
+    match_ = 0;
+    goto end_match_4;
+next_5:;
+    milone_assert(false, 25, 9);
+    match_ = 0;
+    goto end_match_4;
+next_6:;
+end_match_4:;
     return 0;
 }
--- tests/primitives/obj_box_unbox/obj_box_unbox.fs
+++ tests/primitives/obj_box_unbox/obj_box_unbox.fs
@@ -15,7 +15,14 @@ let main _ =
   printIntOrStr true (box 2)
   printIntOrStr false (box "Fizz")
 
-  // boxing int types
+  // These types don't allocate to box.
+  let n = box 42
+  assert (unbox n = 42)
 
+  let xs = box [ 4; 2 ]
+
+  match unbox xs with
+  | [ a; b ] -> assert (a * 10 + b = 42)
+  | _ -> assert false
 
   0
