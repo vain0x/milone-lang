af43f4e439da57decffb36df40d4ca9acad8117f
vain0x <vainzerox@gmail.com>
2021-09-04 22:43:29 +0900

    refactor: Fix a test
    Without `'U`, unbox can't know the type of tuple correctly
    and that program was actually unsound.

--- tests/pendings/fun_monomorphization_bug/fun_monomorphization_bug.c
+++ tests/pendings/fun_monomorphization_bug/fun_monomorphization_bug.c
@@ -1,24 +1,40 @@
 #include "milone.h"
 
+struct IntUnitTuple2_;
+
 struct IntIntTuple2_;
+
+void const* g_(void const* pair_1);
 
 void const* myFst_(struct IntIntTuple2_ pair_);
 
 int milone_main(void);
+
+struct IntUnitTuple2_ {
+    int t0;
+    char t1;
+};
 
 struct IntIntTuple2_ {
     int t0;
     int t1;
 };
 
-void const* myFst_(struct IntIntTuple2_ pair_) {
-    int x_ = pair_.t0;
+void const* g_(void const* pair_1) {
+    int x_ = (*(((struct IntUnitTuple2_ const*)pair_1))).t0;
     return ((void const*)((intptr_t)x_));
+}
+
+void const* myFst_(struct IntIntTuple2_ pair_) {
+    void const* box_ = milone_mem_alloc(1, sizeof(struct IntIntTuple2_));
+    (*(((struct IntIntTuple2_*)box_))) = pair_;
+    void const* call_ = g_(box_);
+    return call_;
 }
 
 int milone_main(void) {
     struct IntIntTuple2_ IntIntTuple2_ = (struct IntIntTuple2_){.t0 = 2, .t1 = 3};
-    void const* call_ = myFst_(IntIntTuple2_);
-    milone_assert((((int)((intptr_t)call_)) == 2), 17, 2);
+    void const* call_1 = myFst_(IntIntTuple2_);
+    milone_assert((((int)((intptr_t)call_1)) == 2), 14, 2);
     return 0;
 }
