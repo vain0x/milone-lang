c902dd158dd80766803c7d3984dda223e17fbe7d
vain0x <vainzerox@gmail.com>
2021-09-12 13:09:14 +0900

    feat: Update profiler on .NET

--- src/MiloneShared/FSharpOnly.fs
+++ src/MiloneShared/FSharpOnly.fs
@@ -50,31 +50,54 @@ let __ptrWrite (_ptr: nativeptr<'a>) (_index: int) (_value: 'a) : unit =
 // -----------------------------------------------
 
 [<NoEquality; NoComparison>]
-type Profiler = Profiler of System.Diagnostics.Stopwatch * int64 ref
+type Profiler =
+  private
+    { Stopwatch: System.Diagnostics.Stopwatch
 
-let private getAllocatedBytes () : int64 =
+      /// Message specified at the time of the previous logging.
+      mutable Msg: string
+
+      /// Elapsed milliseconds at the time of the previous logging.
+      mutable Epoch: int
+
+      /// Heap size (bytes) at the time of the previous logging.
+      mutable Mem: int64 }
+
+let private getAllocatedBytes () =
   System.GC.GetAllocatedBytesForCurrentThread()
 
 let profileInit () : Profiler =
-  let bytesRef = getAllocatedBytes () |> ref
-  Profiler(System.Diagnostics.Stopwatch.StartNew(), bytesRef)
+  { Stopwatch = System.Diagnostics.Stopwatch.StartNew()
+    Msg = "start"
+    Epoch = 0
+    Mem = getAllocatedBytes () }
 
-let profileLog (msg: string) (Profiler (stopwatch, bytesRef)) : unit =
-  let millis = int stopwatch.ElapsedMilliseconds
+let private profilePrint (msg: string) (millis: int) (mem: int64) =
+  let thousandSep (n: int64) =
+    if n < 0L then
+      " -0,000,000"
+    else
+      let k, n = n / 1000L, n % 1000L // kilo
+      let m, k = k / 1000L, k % 1000L // mega
+      sprintf "%3d,%03d,%03d" m k n
+
   let sec = millis / 1000
-  let millis = millis % 1000
+  let millis = millis % 1000 / 10 // per 10 ms
 
-  let totalBytes = getAllocatedBytes ()
+  eprintfn "profile: %-17s time=%4d.%02d mem=%s" msg sec millis (thousandSep mem)
 
-  let bytes =
-    (totalBytes - (!bytesRef)) |> int |> max 0
+let profileLog (msg: string) (p: Profiler) : unit =
+  let epoch = int p.Stopwatch.ElapsedMilliseconds
+  let mem = getAllocatedBytes ()
 
-  let kilo = bytes / 1000
-  let bytes = bytes % 1000
-  let mega = kilo / 1000
-  let kilo = kilo % 1000
+  let epochDelta = epoch - p.Epoch
+  let memDelta = mem - p.Mem
+  profilePrint p.Msg epochDelta memDelta
 
-  eprintfn "profile: time=%4d.%03d mem=%5d,%03d,%03d\n%s" sec millis mega kilo bytes msg
+  p.Msg <- msg
+  p.Epoch <- epoch
+  p.Mem <- mem
 
-  stopwatch.Restart()
-  bytesRef := totalBytes
+  if msg = "Finish" then
+    eprintfn "profile: Finish"
+    profilePrint "total" epoch mem
