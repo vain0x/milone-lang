a1de2dbda790757b998163586146fa28080d57a9
vain0x <vainzerox@gmail.com>
2021-11-14 18:18:21 +0900

    refactor: Remove unused code

--- src/MiloneLspServer/LspUtil.fs
+++ src/MiloneLspServer/LspUtil.fs
@@ -1,8 +1,6 @@
 module rec MiloneLspServer.LspUtil
 
-open MiloneShared.SharedTypes
 open MiloneShared.Util
-open MiloneLspServer.Util
 open MiloneStd.StdMap
 
 module S = MiloneStd.StdString
@@ -147,49 +145,3 @@ let aggregateDiagnostics diagnosticsKeys diagnosticsCache diagnostics =
     |> DiagnosticsCache.filter diagnostics
 
   diagnostics, diagnosticsKeys, diagnosticsCache
-
-// -----------------------------------------------
-// Docs
-// -----------------------------------------------
-
-/// Text doc that is opened in editor.
-[<RequireQualifiedAccess; NoEquality; NoComparison>]
-type DocData =
-  { /// Number to identify an edition of the document.
-    Version: int
-    Text: string }
-
-type Docs = TreeMap<Uri, DocData>
-
-module Docs =
-  let empty: Docs = TMap.empty Uri.compare
-
-  let private find (uri: Uri) (docs: Docs) : DocData option =
-    traceFn "Doc find uri:'%A'" (string uri)
-    docs |> TMap.tryFind uri
-
-  let getVersion (uri: Uri) docs : int =
-    match docs |> find uri with
-    | Some d -> d.Version
-    | None -> 0
-
-  let getText (uri: Uri) docs : string option =
-    match docs |> find uri with
-    | Some d -> Some d.Text
-    | None -> None
-
-  let add (uri: Uri) (version: int) (text: string) (docs: Docs) =
-    let docData: DocData = { Version = version; Text = text }
-
-    debugFn "Doc opened uri:'%A' v:%d len:%d" uri version text.Length
-    docs |> TMap.add uri docData
-
-  let update (uri: Uri) (version: int) (text: string) (docs: Docs) : Docs =
-    let docData: DocData = { Version = version; Text = text }
-
-    debugFn "Doc changed uri:'%A' v:%d len:%d" uri version text.Length
-    docs |> TMap.add uri docData
-
-  let remove (uri: Uri) (docs: Docs) : Docs =
-    debugFn "Doc closed uri:'%A'" uri
-    docs |> TMap.remove uri |> snd
