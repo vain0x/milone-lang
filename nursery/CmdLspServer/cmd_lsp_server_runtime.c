7f2cf7afdf16c4ffd5f52b9bd9191560b635d010
vain0x <vainzerox@gmail.com>
2021-11-29 01:34:07 +0900

    feat: Publish diagnostics, running `milone check`

--- nursery/CmdLspServer/cmd_lsp_server_runtime.c
+++ nursery/CmdLspServer/cmd_lsp_server_runtime.c
@@ -2,9 +2,11 @@
 #include <assert.h>
 #include <dirent.h>
 #include <errno.h>
+#include <signal.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include <milone.h>
@@ -49,4 +51,84 @@ REDO:;
 
     *name = str_of_c_str(entry->d_name);
     return true;
+}
+
+// -----------------------------------------------
+// process
+// -----------------------------------------------
+
+struct Subprocess {
+    pid_t pid;
+    bool alive;
+    int exit_code;
+    FILE *stdin_file;
+    FILE *stdout_file;
+};
+
+struct StringCons {
+    struct String head;
+    struct StringCons *tail;
+};
+
+// Execute a subprocess and retrieves stdout and exit code.
+int milone_subprocess_run2(struct String command, struct StringCons *args,
+                          struct String *out_stdout) {
+    assert(out_stdout != NULL);
+
+    int stdout_fd[2];
+    if (pipe(stdout_fd) != 0) {
+        assert(false && "pipe(stdout)");
+    }
+
+    pid_t child = fork();
+    assert(child != -1);
+
+    if (child == 0) {
+        // In child process:
+
+        close(stdout_fd[0]);
+        dup2(stdout_fd[1], STDOUT_FILENO);
+
+        // cmd:
+        char *cmd = (char *)str_to_c_str(command);
+
+        // args:
+        char *arg_array[16] = {cmd};
+        int i = 1;
+        for (struct StringCons *p = args; p != NULL; p = p->tail) {
+            arg_array[i] = (char *)str_to_c_str(p->head);
+            i++;
+            assert(i < 15 && "too many args");
+        }
+
+        exit(execv(cmd, arg_array));
+    }
+
+    // In parent process:
+
+    // stdio:
+    close(stdout_fd[1]);
+    FILE *stdout_file = fdopen(stdout_fd[0], "rb");
+
+    // Read stdout. (trimmed to capacity)
+    char buf[8000] = "";
+    size_t i = 0;
+    while (true) {
+        size_t remain = sizeof(buf) - 1 - i;
+        size_t read_size = fread(buf + i, 1, remain, stdout_file);
+        if (read_size == 0) {
+            break;
+        }
+        i += read_size;
+    }
+
+    *out_stdout = str_of_raw_parts(buf, i);
+
+    // Join.
+    int status;
+    pid_t result = waitpid(child, &status, 0);
+    assert(result == child);
+    int exit_code = WIFEXITED(status) ? WEXITSTATUS(status) : EXIT_FAILURE;
+
+    return exit_code;
 }
