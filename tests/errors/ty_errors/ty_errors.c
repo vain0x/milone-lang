7762c57542094c8886250e9aa19285c921612536
vain0x <vainzerox@gmail.com>
2023-01-23 00:19:39 +0900

    chore: Commit generated code

--- tests/errors/ty_errors/ty_errors.c
+++ tests/errors/ty_errors/ty_errors.c
@@ -1,4 +1,4 @@
 #error ty_errors.ty_errors:3:1 Type mismatch: 'unit' <> 'int'.
 #error ty_errors.ty_errors:6:11 Type mismatch: 'int' <> 'string'.
 #error ty_errors.ty_errors:12:18 Arity mismatch: expected (((_) -> _) -> _, ..) -> .., but was (_) -> _.
-#error ty_errors.ty_errors:12:19 Recursive type occurred while unifying '{?17}@ty_errors.ty_errors:12:13 -> {?18}@ty_errors.ty_errors:12:18' to '{?22}@ty_errors.ty_errors:12:18'.
+#error ty_errors.ty_errors:12:19 Recursive type occurred while unifying '?17 -> ?18' to '?22'.
